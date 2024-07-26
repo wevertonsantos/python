94beabe1caccf4a28ed519c0485a1118915e4098
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-07-25 21:25:28 -0300

    float, round, variavel + decimal

--- calculadora.py
+++ calculadora.py
@@ -1,4 +1,14 @@
 # python suporta (+ = * / %)
-x = int(input("What's x? "))
+x = int(input("What's x? ")) #variável tipo inteiro
 y = int(input("What's y? "))
 print(x + y)
+
+a = float(input("What's a? ")) #variável tipo float
+b = float(input("What's b? "))
+c = round(a + b) #https://docs.python.org/3/library/functions.html#round
+print(f"{c:,}") #formatando float para adicionar (.) a milhar
+
+d = round(a / b, 2) # aredondando com duas casas decimais
+print(d)
+d = a / b
+print(f"{d:.2f}") # aredondando com duas casas decimais
