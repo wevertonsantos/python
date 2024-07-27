a520bdeba96a29f64c8e51266016e494c8d1a43e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-07-26 22:06:35 -0300

    função com retorno

--- def.py
+++ def.py
@@ -1,8 +1,14 @@
 def main(): # função main
     name = input("What's your name? ")
     hello(name)
+    x = int(input("What's x? "))
+    y = int(input("What's y? "))
+    caculator(x,y)
 
 def hello(name="world"): #função com argumento padrão
     print(f"Hello, {name}")
 
-main() #chamando a função para executar a pilha de código
+def caculator(x,y): # função com retorno
+    return print(x + y)
+
+main() #chamando a função para executar a pilha de códigos 
