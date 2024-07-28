7989314993a1b890f102004415df65cd6eed9b6f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-07-27 21:08:21 -0300

    side effects

---
+++ havard-cs50p/machine.py
@@ -0,0 +1,12 @@
+emoji = "?.?"
+
+def main():
+    global emoji #variável global acessível dentro da função para alterar o valor da variável
+    say("Is anyone there?")
+    emoji = "=)"
+    say("Oh, hey!")
+
+def say(phrase):
+    print(f"{phrase} {emoji}")
+
+main()
