4812acb0f4c2527945eac0807ce3c167a3d1f1ba
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-05 20:08:19 -0300

    if com elif

--- harvard-cs50p/conditionals/compare.py
+++ harvard-cs50p/conditionals/compare.py
@@ -12,7 +12,7 @@ y = int(input("What's y? "))
 
 if x < y:
     print(f"{x} is less than {y}")
-if x > y:
+elif x > y:
     print(f"{x} is greater than {y}")
-if x == y:
+elif x == y:
     print(f"{x} is equal to {y}")
