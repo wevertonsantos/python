3f9ad28ecb735804751c68c391b6e790649f8b56
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-08 21:24:56 -0300

    testes com algumas regras

--- data-structures-and-algorithms/binary-search-linked-lists-and-complexity/problem-cards.py
+++ data-structures-and-algorithms/binary-search-linked-lists-and-complexity/problem-cards.py
@@ -50,23 +50,72 @@ tests.append({
     'output': -1
 })
 
+tests.append({
+    'input': {
+        'cards': [-8, 11, -23, 7, 12, 4, 2, 0],
+        'query': 0
+    },
+    'output': 7
+})
+
+tests.append({
+    'input': {
+        'cards': [6, 6, 6, 7, 7, 4, 4, 4,6,6],
+        'query': 7
+    },
+    'output': 3
+})
+
+tests.append({
+    'input': {
+        'cards': [50],
+        'query': 50
+    },
+    'output': 0
+})
+
+tests.append({
+    'input': {
+        'cards': [],
+        'query': 7
+    },
+    'output': -1
+})
+
 #We will assume that our function will return -1 in case cards does not contain query
 def locate_card(cards,query):
     for i in range(len(cards)):
-        if cards.__contains__(query):
+        if cards.__contains__(query): #caso contém query
             if cards.index(query) == i:
                 return i
         else: return -1
 
-#efetuando testes
+# query occurs in the middle
 cards = tests[0]['input']['cards']
-print(locate_card(cards,7)) # return 3
-
+print(locate_card(cards,7)) # retorna 3
 cards = tests[1]['input']['cards']
-print(locate_card(cards,32)) #return 4
+print(locate_card(cards,32)) #retorna 4
 
+# query is the first element
 cards = tests[2]['input']['cards']
-print(locate_card(cards,-23)) #return 0
+print(locate_card(cards,-23)) #retorna 0
 
+# cards does not contain query 
 cards = tests[3]['input']['cards']
-print(locate_card(cards,900)) #return -1
+print(locate_card(cards,900)) #retorna -1
+
+# query is the last element
+cards = tests[4]['input']['cards']
+print(locate_card(cards,0)) #retorna 7
+
+# numbers can repeat in cards
+cards = tests[5]['input']['cards']
+print(locate_card(cards,7)) #retorna 3
+
+# cards contains just one element, query
+cards = tests[6]['input']['cards'] #retorna 0
+print(locate_card(cards,50))
+
+# cards is empty
+cards = tests[7]['input']['cards'] #SOLUCIONAR tem que retornar -1
+print(locate_card(cards,7))
