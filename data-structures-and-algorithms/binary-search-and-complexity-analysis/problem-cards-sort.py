fa128e1d104a34be6b6d114da88a9179b0a6d959
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-10 20:27:40 -0300

    buscando a solução correta com binary search

--- data-structures-and-algorithms/binary-search-and-complexity-analysis/problem-cards-sort.py
+++ data-structures-and-algorithms/binary-search-and-complexity-analysis/problem-cards-sort.py
@@ -11,13 +11,30 @@ If no more elements remain, return -1.
 '''
 cards = [9,7,6,4,3,2,1]
 
-#9,7,6,4,3,2,1
-
 def locate_cards(cards,query):
-    #precisa verificar o elemento do meio
-    element_middle = (len(cards) - 1) / 2
-    for i in range(len(cards)):
-        #se é igual a query
-        print(i)
+    first_half_list = []
+    second_half_list = []
+    #precisa encontrar o elemento do meio
+    element_middle = int((len(cards) - 1) / 2)
+    #se o elemento do meio é igual a query
+    if element_middle == query:
+        #retorna a posição do meio
+        return cards[element_middle]
+    #se elemento do meio é menor que a query
+    elif element_middle < query:
+        #procura a primeira parte da lista
+        for i in range(0,element_middle):
+            first_half_list.append(i)
+        if first_half_list[query]:
+            ...
+    #se elemento do meio for maior que a query
+    elif cards[element_middle] > query: # pegar o elemento antes da query
+         for i in range(element_middle,len(cards)):
+            #procura a secunda parte da lista
+            second_half_list.append(i)
+            return second_half_list[query]
+    else:
+         return -1
 
-locate_cards(cards,6)
+res = locate_cards(cards,6)
+print(res)
