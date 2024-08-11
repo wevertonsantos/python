60d1cb405d37e8c7ce74921a015df99df0e555da
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-11 16:01:20 -0300

    recomendação mais limpa

---
+++ harvard-cs50p/conditionals/recommendations2.py
@@ -0,0 +1,29 @@
+def main():
+    difficulty = input("Difficult or Casual? ")
+    
+    if difficulty == "Difficult" or difficulty == "Casual":
+        players = input("Multiplayer or Single-player? ")
+        if players == "Multiplayer" or players == "Single-player":
+            choose(difficulty, players)
+        else:
+            print("Enter a valid number of players")
+
+    else:
+        print("Enter a valid difficulty")
+
+def choose(difficulty, players):
+        if difficulty == "Difficult":
+            if players == "Multiplayer":
+                recommend("Poker")
+            else:
+                recommend("Klondike")    
+        else:
+            if players == "Multiplayer":
+                recommend("Hearts")
+            else:
+                recommend("Clock")
+
+def recommend(game):
+    print("You might like", game)
+
+main()
