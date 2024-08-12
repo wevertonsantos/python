72b03b90f389e180f33cfac95e7bc7445cd8bc08
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-12 20:53:38 -0300

    while statement

---
+++ harvard-cs50p/loops/cat.py
@@ -0,0 +1,10 @@
+i = 3
+while i != 0:# 3 é diferente de 0?
+    print("meow")#printa
+    i -= 1# 3 - 1 = 2
+    # 0 é diferente de 0? não, é igual, então para
+
+i = 0
+while i < 3:
+    print("rss")
+    i += 1
