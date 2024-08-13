6d43c541f701a11379fe3406aa0a194a0c0da5c9
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-13 20:24:35 -0300

    aprendendo sobre lista

---
+++ harvard-cs50p/loops/list.py
@@ -0,0 +1,15 @@
+students = ["I","you", "we"]
+
+#index 0 no array
+print(students[0])
+print(students[1])
+print(students[2])
+
+for student in students:
+    print(student)
+
+fruits = ["Apple","Orange","Limon"]
+
+for i in range(len(fruits)):
+    print(i)
+    print(fruits[i])
