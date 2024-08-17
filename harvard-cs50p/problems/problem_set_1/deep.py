0d0c2ec60a274e58508d9594300f120a0e213992
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-16 22:07:44 -0300

    solved - problem deep

---
+++ harvard-cs50p/problems/problem_set_1/deep.py
@@ -0,0 +1,9 @@
+def main():
+    answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
+    result(answer)
+
+def result(answer):
+    answer = answer.lower().strip()
+    print("Yes") if answer == str(42) or answer == "forty-two" or answer == "forty two" else print("No")
+
+main()
