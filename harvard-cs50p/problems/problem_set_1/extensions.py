7521d6efcfb133011ab23d55b8ab377f5e4dc56b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-18 22:40:48 -0300

    finalizando problema

--- harvard-cs50p/problems/problem_set_1/extensions.py
+++ harvard-cs50p/problems/problem_set_1/extensions.py
@@ -8,10 +8,10 @@ def media_type(file_name):
     extensions = [".gif",".jpg",".jpeg",".png",".pdf",".txt",".zip"]
     for extension in extensions:
         if extension in file_name:
-            if file_name.endswith(".txt"):
+            if extension == ".txt":
                 return "text/plain"
-            elif file_name.endswith(".zip") or file_name.endswith(".pdf"):
-                return "application/zip" if file_name.endswith(".zip") else "application/pdf"
+            elif extension == ".zip" or extension == ".pdf":
+                return "application/zip" if extension == ".zip" else "application/pdf"
             else:
                 return f"image/{extension.replace(".","")}" if not file_name.endswith(".jpg") else "image/jpeg"
     else:
