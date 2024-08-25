bc7ac21f7c8f442f4842ec79742c183e7af9baed
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-25 20:36:58 -0300

    i'm starting the problem

--- harvard-cs50p/problems/problem_set_2/camel.py
+++ harvard-cs50p/problems/problem_set_2/camel.py
@@ -7,10 +7,17 @@ def main():
 def camel_case(name):
     ...
 
-main()
+#main()
 
-name = "caMelCase"
-for _ in name:
-    ...
-if name == name.capitalize():
-    print()
+#Pegar a string da variável
+name = "camelCase"
+words_capitalize = ""
+words_lower = ""
+for n in name:
+    #Pegar a letra que está em maiúscula
+    if n.isupper():
+        #Pegar da letra maiúscula até o restante da string
+        if name[name.index(n):] not in words_capitalize:
+            words_capitalize = name[name.index(n):]
+            words_lower = name[:name.index(n)]
+print(f"{words_lower}_{words_capitalize.lower()}")
