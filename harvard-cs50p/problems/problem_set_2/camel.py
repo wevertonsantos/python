132c9c792b4318eb5df66e658589a4ba449cf620
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-25 21:31:29 -0300

    finalizando

--- harvard-cs50p/problems/problem_set_2/camel.py
+++ harvard-cs50p/problems/problem_set_2/camel.py
@@ -10,19 +10,29 @@ def camel_case(name):
 #main()
 
 #Pegar a string da variável
-name = "camelCaseAeseCase"
+name = "camel"
 words_capitalize = ""
 word_lower = ""
 snake_case = ""
+first_time = False
 for n in name:
     #Pegar a letra que está em maiúscula
-    if n.isupper():
-        word_capitalize = name[name.index(n):]
-        #Pegar da letra maiúscula até o restante da string
-        if word_capitalize not in words_capitalize:
-            for word in word_capitalize:
-                #Se a próxima palavra for maiúscula, colocar um "_" antes dela 
-                words_capitalize += word
-                # A cada palavra com a primeira letra maiúscula, colocar "_"
+    if first_time == False:
+        if n.isupper():
+            word_lower = name[:name.index(n)]
+            word_capitalize = name[name.index(n):]
+            #Pegar da letra maiúscula até o restante da string
+            if word_capitalize not in words_capitalize:
+                for word in word_capitalize:
+                    #Se a próxima palavra for maiúscula, colocar um "_" antes dela 
+                    if not word.isupper():
+                        words_capitalize += word.lower()
+                    else:
+                        words_capitalize += f"_{word.lower()}"
+            first_time = True
 
+if words_capitalize.isspace():
+    snake_case = f"{word_lower}"
+else:
+    snake_case = f"{word_lower}{words_capitalize}"
 print(snake_case)
