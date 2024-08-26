51a52ac7d948c76df3007e77f4ecae0c4e6296ca
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-26 20:32:11 -0300

    finalizing, next step is refactor

--- harvard-cs50p/problems/problem_set_2/camel.py
+++ harvard-cs50p/problems/problem_set_2/camel.py
@@ -5,34 +5,32 @@ def main():
     print(camel_case(name))
 
 def camel_case(name):
-    ...
+    first_time = False
+    words_capitalize = ""
+    word_lower = ""
+    snake_case = ""
+    for n in name:
+        if first_time == False:
+            if name.islower():
+                word_lower = name
+            #Pegar a letra que está em maiúscula
+            else: 
+                word_capitalize = name[name.index(n):]
+                word_lower = name[:name.index(n)]
+                #Pegar da letra maiúscula até o restante da string
+                if word_capitalize not in words_capitalize:
+                    for word in word_capitalize:
+                        #Se a próxima palavra for maiúscula, colocar um "_" antes dela 
+                        if not word.isupper():
+                            words_capitalize += word.lower()
+                        else:
+                            words_capitalize += f"_{word.lower()}"
+                first_time = True
+    if words_capitalize == "":
+        snake_case = word_lower
+        return snake_case
+    else:
+        snake_case = f"{word_lower}{words_capitalize}"
+        return snake_case
 
-#main()
-
-#Pegar a string da variável
-name = "camel"
-words_capitalize = ""
-word_lower = ""
-snake_case = ""
-first_time = False
-for n in name:
-    #Pegar a letra que está em maiúscula
-    if first_time == False:
-        if n.isupper():
-            word_lower = name[:name.index(n)]
-            word_capitalize = name[name.index(n):]
-            #Pegar da letra maiúscula até o restante da string
-            if word_capitalize not in words_capitalize:
-                for word in word_capitalize:
-                    #Se a próxima palavra for maiúscula, colocar um "_" antes dela 
-                    if not word.isupper():
-                        words_capitalize += word.lower()
-                    else:
-                        words_capitalize += f"_{word.lower()}"
-            first_time = True
-
-if words_capitalize.isspace():
-    snake_case = f"{word_lower}"
-else:
-    snake_case = f"{word_lower}{words_capitalize}"
-print(snake_case)
+main()
