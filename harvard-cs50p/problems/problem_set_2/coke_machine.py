371fd016dcafa8bf96d46728ed8a5a2efadc86af
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-27 20:51:10 -0300

    mudando comentario

--- harvard-cs50p/problems/problem_set_2/coke_machine.py
+++ harvard-cs50p/problems/problem_set_2/coke_machine.py
@@ -1,6 +1,6 @@
 #https://cs50.harvard.edu/python/2022/psets/2/coke/
 
-#Se a soma das coins que o user inseriu não é da quantia do valor da garrafa, mostrar o amontoado, valor devido e fazer com que chegue até o valor devido ser igual a 0
+#Se a soma das coins que o user inseriu não é da quantia do valor da garrafa, valor devido e fazer com que chegue até o valor devido ser igual a 0
 
 amount_due = 50
 coin = 0
