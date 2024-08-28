af63f74300ccc8c136df2988ec5c4a9b7fbb4505
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-28 20:24:32 -0300

    i've finished coke problem

--- harvard-cs50p/problems/problem_set_2/coke.py
+++ harvard-cs50p/problems/problem_set_2/coke.py
@@ -1,18 +1,21 @@
 #https://cs50.harvard.edu/python/2022/psets/2/coke/
 
-#Se a soma das coins que o user inseriu não é da quantia do valor da garrafa, o valor devido deve chegar até ser igual a 0
-
 amount_due = 50
 coin = 0
-while amount_due != 0:
+while amount_due > 0:
     print(f"Amount Due: {amount_due}")
     coin = input("Insert Coin: ")
     if coin.isdigit():
         coin = int(coin)
         if coin == 25 or coin == 10 or coin == 5:
-            amount_due -= coin
+                if coin < amount_due:
+                    amount_due -= coin
+                else:
+                     amount_due = coin - amount_due
+                     print(f"Change Owed: {amount_due}")
+                     break
         else:
-            print("Enter a valid coin")
+            print(f"Amount Due: {amount_due}")
             break
     else:
         print("Enter a integer")
