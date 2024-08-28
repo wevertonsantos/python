95bdd15de53181032e21dbe2ab18b6923e185327
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-27 21:07:39 -0300

    i finished the problem

--- harvard-cs50p/problems/problem_set_2/coke_machine.py
+++ harvard-cs50p/problems/problem_set_2/coke_machine.py
@@ -6,7 +6,16 @@ amount_due = 50
 coin = 0
 while amount_due != 0:
     print(f"Amount Due: {amount_due}")
-    coin = int(input("Insert Coin: "))
-    amount_due -= coin
+    coin = input("Insert Coin: ")
+    if coin.isdigit():
+        coin = int(coin)
+        if coin == 25 or coin == 10 or coin == 5:
+            amount_due -= coin
+        else:
+            print("Enter a valid coin")
+            break
+    else:
+        print("Enter a integer")
+        break
 else:
     print(f"Change Owed: {amount_due}")
