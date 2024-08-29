544c6f7e6c56a20e3e45dd8cd77925d75ceb7861
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-29 20:35:34 -0300

    starting problem

---
+++ harvard-cs50p/problems/problem_set_2/twttr.py
@@ -0,0 +1,10 @@
+#https://cs50.harvard.edu/python/2022/psets/2/twttr/
+
+def main():
+    text = input("Input: ").strip()
+    print(omitting_vowels(text))
+
+def omitting_vowels(text):
+    ...
+
+main()
