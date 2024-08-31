c5acb91c02779a3cc585b26e885e7ecb73a1e4d6
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-31 20:41:26 -0300

    continuação da resolução

--- harvard-cs50p/problems/problem_set_2/plates.py
+++ harvard-cs50p/problems/problem_set_2/plates.py
@@ -10,31 +10,37 @@ def is_valid(s):
 s = "AAA222"
 sum_str = 0
 middle_element = 0
+
+def invalid_things(s):
+    invalid_things = [".",";"," ","!",",","?"]
+    for invalid_thing in invalid_things:
+        return False if invalid_thing not in s else True
+index = int()
+ads = s[int(len(s) / 3)]
 if len(s) == 6 or len(s) > 2:
-    for i in range(len(s)):
-        #se placa começa com duas letras
-        if i < 2:
-            if s[i].isnumeric():
+    numbers = []
+    if invalid_things(s) == False:
+        for i in range(len(s)):
+            if s[i].isnumeric(): numbers.append(s[i])
+            if i < 2:
+                if s[i].isnumeric():
+                    print("Invalid")
+                    break
+            elif s[int(len(s) / 3)].isnumeric():
                 print("Invalid")
                 break
-        elif middle_element:
-            if len(s) % 2 != 0:
-                sum_str += i
-                middle_element = sum_str / len(s)
-                if not middle_element.is_integer():
+            elif not numbers == []: 
+                if int(numbers[0]) == 0:
                     print("Invalid")
-                else:
-                    print("Valid")
-            
-
+                    break
+            else:
+                print("Invalid")
+                break
+        else:
+            print("Valid")
+    else:
+        print("Invalid")
 else:
     print("Invalid")
 
 #main()
-
-'''
-if not middle_element.is_integer():
-    print("Invalid")
-else:
-    print("Valid")
-'''
