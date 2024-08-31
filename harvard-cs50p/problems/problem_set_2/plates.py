6abeb4fcbf95eaae9834c765edfe3e61caa97643
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-30 22:42:13 -0300

    primeiras linhas

--- harvard-cs50p/problems/problem_set_2/plates.py
+++ harvard-cs50p/problems/problem_set_2/plates.py
@@ -7,18 +7,34 @@ def main():
 def is_valid(s):
     ...
 
-s = "CS50"
+s = "AAA222"
+sum_str = 0
+middle_element = 0
 if len(s) == 6 or len(s) > 2:
     for i in range(len(s)):
         #se placa começa com duas letras
         if i < 2:
-            if not s[i].isnumeric():
+            if s[i].isnumeric():
                 print("Invalid")
-                if len(s) % 2 == 0:
-                    len_str = len(s) / 2
+                break
+        elif middle_element:
+            if len(s) % 2 != 0:
+                sum_str += i
+                middle_element = sum_str / len(s)
+                if not middle_element.is_integer():
+                    print("Invalid")
                 else:
-                    len_str = len_str(s)
+                    print("Valid")
+            
+
 else:
     print("Invalid")
 
-main()
+#main()
+
+'''
+if not middle_element.is_integer():
+    print("Invalid")
+else:
+    print("Valid")
+'''
