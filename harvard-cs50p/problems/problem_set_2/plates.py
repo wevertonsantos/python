9b1ac294b0f7f57a79ec786730b2ce77f53ab5e8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-31 21:14:14 -0300

    melhorando código

--- harvard-cs50p/problems/problem_set_2/plates.py
+++ harvard-cs50p/problems/problem_set_2/plates.py
@@ -5,38 +5,32 @@ def main():
     print("Valid") if is_valid(plate) else print("Invalid")
 
 def is_valid(s):
-    ...
-
-s = "CS50"
-sum_str = 0
-middle_element = 0
+    if len(s) == 6 or len(s) > 2:
+        numbers = []
+        if invalid_things(s) == False:
+            for i in range(len(s)):
+                if s[i].isnumeric(): numbers.append(s[i])
+                if i < 2:
+                    if s[i].isnumeric():
+                        return False
+                        break
+                elif not s[-1].isnumeric():
+                    return False
+                    break
+                elif not numbers == []: 
+                    if int(numbers[0]) == 0:
+                        return False
+                        break
+            else:
+                return True
+        else:
+            return False
+    else:
+        return False
 
 def invalid_things(s):
     invalid_things = [".",";"," ","!",",","?"]
     for invalid_thing in invalid_things:
         return False if invalid_thing not in s else True
 
-if len(s) == 6 or len(s) > 2:
-    numbers = []
-    if invalid_things(s) == False:
-        for i in range(len(s)):
-            if s[i].isnumeric(): numbers.append(s[i])
-            if i < 2:
-                if s[i].isnumeric():
-                    print("Invalid")
-                    break
-            elif not s[-1].isnumeric():
-                print("Invalid")
-                break
-            elif not numbers == []: 
-                if int(numbers[0]) == 0:
-                    print("Invalid")
-                    break
-        else:
-            print("Valid")
-    else:
-        print("Invalid")
-else:
-    print("Invalid")
-
-#main()
+main()
