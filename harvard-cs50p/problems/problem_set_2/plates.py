536b35cfd6153898ee96ae8bfee9cdfd1decbeb8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-08-31 21:07:39 -0300

    finalizando algoritmo

--- harvard-cs50p/problems/problem_set_2/plates.py
+++ harvard-cs50p/problems/problem_set_2/plates.py
@@ -7,7 +7,7 @@ def main():
 def is_valid(s):
     ...
 
-s = "AAA222"
+s = "CS50"
 sum_str = 0
 middle_element = 0
 
@@ -15,8 +15,7 @@ def invalid_things(s):
     invalid_things = [".",";"," ","!",",","?"]
     for invalid_thing in invalid_things:
         return False if invalid_thing not in s else True
-index = int()
-ads = s[int(len(s) / 3)]
+
 if len(s) == 6 or len(s) > 2:
     numbers = []
     if invalid_things(s) == False:
@@ -26,16 +25,13 @@ if len(s) == 6 or len(s) > 2:
                 if s[i].isnumeric():
                     print("Invalid")
                     break
-            elif s[int(len(s) / 3)].isnumeric():
+            elif not s[-1].isnumeric():
                 print("Invalid")
                 break
             elif not numbers == []: 
                 if int(numbers[0]) == 0:
                     print("Invalid")
                     break
-            else:
-                print("Invalid")
-                break
         else:
             print("Valid")
     else:
