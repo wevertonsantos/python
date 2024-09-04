9e252336a7e141c1e4a72b5a1a305aa3372cbdba
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-03 21:06:32 -0300

    função compacta

--- harvard-cs50p/exceptions/number.py
+++ harvard-cs50p/exceptions/number.py
@@ -5,10 +5,8 @@ def main():
 def get_int():
     while True:
         try:
-            x = int(input("What's x? "))
+            return int(input("What's x? "))
         except ValueError:
-            print("x is not an integer")
-        else:
-            return x       
+            print("x is not an integer")   
 
 main()
