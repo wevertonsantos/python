d7c0989a714d09050bd3fcd422ae4ae228ddfe4b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-07 20:01:39 -0300

    i've been finishing the problem

--- harvard-cs50p/problems/problem_set_3/fuel.py
+++ harvard-cs50p/problems/problem_set_3/fuel.py
@@ -1,20 +1,28 @@
-#https://cs50.harvard.edu/python/2022/psets/3/fuel/
-def main ():
-    fraction = input("Fraction: ")
-    print(fuel(fraction))
+# https://cs50.harvard.edu/python/2022/psets/3/fuel/
+def main():
+    print(fuel())
 
-def fuel(fraction):
+def fuel():
+    fraction = input("Fraction: ")
     while True:
         try:
-            x = int(fraction[0])
-            y = int(fraction[2])
+            x = fraction[0]
+            y = fraction[2]
+            if x > y or y == '0':
+                x = int(x)
+                y = int(y)
+        except ValueError:
+            fraction = input("Fraction: ")
+        else:
             if f"{x}/{y}" == "1/4":
                 return "25%"
             elif f"{x}/{y}" == "1/2":
                 return "50%"
             elif f"{x}/{y}" == "3/4":
                 return "75%"
-        except ValueError:
-            fraction = input("Fraction: ")
+            elif f"{x}/{y}" == "4/4":
+                return "F"
+            else:
+                return "E"
 
 main()
