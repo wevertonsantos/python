8049cb4c7aba19ba6c2554861f805fbffedee702
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-07 20:43:04 -0300

    i'd finished this problem

--- harvard-cs50p/problems/problem_set_3/fuel.py
+++ harvard-cs50p/problems/problem_set_3/fuel.py
@@ -5,24 +5,30 @@ def main():
 def fuel():
     fraction = input("Fraction: ")
     while True:
-        try:
+        if '.' not in fraction:
             x = fraction[0]
             y = fraction[2]
-            if x > y or y == '0':
+            try:
                 x = int(x)
                 y = int(y)
-        except ValueError:
-            fraction = input("Fraction: ")
-        else:
-            if f"{x}/{y}" == "1/4":
-                return "25%"
-            elif f"{x}/{y}" == "1/2":
-                return "50%"
-            elif f"{x}/{y}" == "3/4":
-                return "75%"
-            elif f"{x}/{y}" == "4/4":
-                return "F"
+                if y == 0 or x > y:
+                    result = x / 0
+            except ValueError:
+                fraction = input("Fraction: ")
+            except ZeroDivisionError:
+                fraction = input("Fraction: ")
             else:
-                return "E"
+                if f"{x}/{y}" == "1/4":
+                    return "25%"
+                elif f"{x}/{y}" == "1/2":
+                    return "50%"
+                elif f"{x}/{y}" == "3/4":
+                    return "75%"
+                elif f"{x}/{y}" == "4/4":
+                    return "F"
+                else:
+                    return "E"
+        else:
+            fraction = input("Fraction: ")
 
 main()
