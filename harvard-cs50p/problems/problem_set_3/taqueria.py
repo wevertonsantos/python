292442e878c53bda93ec69e0f6d684e1f7807767
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-08 19:35:32 -0300

    finished

--- harvard-cs50p/problems/problem_set_3/taqueria.py
+++ harvard-cs50p/problems/problem_set_3/taqueria.py
@@ -1,21 +1,6 @@
 #https://cs50.harvard.edu/python/2022/psets/3/taqueria/
 
 def main():
-    print(total_cost())
-
-menu = {
-    "Baja Taco": 4.25,
-    "Burrito": 7.50,
-    "Bowl": 8.50,
-    "Nachos": 11.00,
-    "Quesadilla": 8.50,
-    "Super Burrito": 8.50,
-    "Super Quesadilla": 9.50,
-    "Taco": 3.00,
-    "Tortilla Salad": 8.00
-}
-
-def total_cost():
     item = input("Item: ").strip().title()
     cost = 0
     while True:
@@ -28,7 +13,21 @@ def total_cost():
                     item = input("Item: ").strip().title()
             if item == '':
                 item = int(item)
+            else:
+                item = input("Item: ").strip().title()
         except ValueError:
             exit()
+
+menu = {
+    "Baja Taco": 4.25,
+    "Burrito": 7.50,
+    "Bowl": 8.50,
+    "Nachos": 11.00,
+    "Quesadilla": 8.50,
+    "Super Burrito": 8.50,
+    "Super Quesadilla": 9.50,
+    "Taco": 3.00,
+    "Tortilla Salad": 8.00
+}
 
 main()
