96059d0ec657f9b8fc89b3ab7f9b491bb8e7921c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-11 19:51:36 -0300

    starting the problem

---
+++ harvard-cs50p/problems/problem_set_3/grocery.py
@@ -0,0 +1,13 @@
+#https://cs50.harvard.edu/python/2022/psets/3/grocery/
+
+def main():
+    items = input()
+    grocery_list(items)
+
+def grocery_list(items):
+    list_items = []
+    list_items.append(items)
+    list_items.sort
+    print(list_items)
+
+main()
