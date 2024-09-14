aa4a6de2da0924f1b21785c41978dc93937a53e3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-13 23:26:07 -0300

    buscando solução aos poucos

--- harvard-cs50p/problems/problem_set_3/outdated.py
+++ harvard-cs50p/problems/problem_set_3/outdated.py
@@ -5,39 +5,52 @@ def main():
     formatter_date(date)
 
 def formatter_date(date):
-    #Verificar os dados que estão contido na data
-    #Pegar o dia, mês e ano da data
     ...
 
-months = [
-    "January",
-    "February",
-    "March",
-    "April",
-    "May",
-    "June",
-    "July",
-    "August",
-    "September",
-    "October",
-    "November",
-    "December"
-]
+months = {
+    "January":1,
+    "February":2,
+    "March":3,
+    "April":4,
+    "May":5,
+    "June":6,
+    "July":7,
+    "August:":8,
+    "September":9,
+    "October":10,
+    "November":11,
+    "December":12
+}
 
 #main()
 
-date = "September 8, 1636"
+date = "December 80, 1980"
 if '/' in date and len(date) == 8:
-    month = month = int(date[0])
+    month = int(date[0])
     year = int(date[4:])
     day = int(date[2])
-    if month < 10:
-        month = f"0{int(date[0])}"
-    if day < 10:
-        day = f"0{int(date[2])}"
-    date = f"{year}-{month}-{day}"
+    if day <= 31:
+        if month < 10:
+            month = f"0{month}"
+        if day < 10:
+            day = f"0{day}"
+        date = f"{year}-{month}-{day}"
     print(date)
 else:
     for month in months:
         if month in date:
-            ...
+            if date.startswith(month):
+                if len(date) == 17:
+                    month = int(months[month])
+                    year = int(date[13:])
+                    day = int(date[10])
+                    if day <= 31:
+                        if month < 10:
+                            month = f"0{month}"
+                        if day < 10:
+                            day = f"0{day}"
+                        date = f"{year}-{month}-{day}"
+                    print(date)
+                    #print(months[month])
+                    #print(date[10])
+                    #print(date[13:])
