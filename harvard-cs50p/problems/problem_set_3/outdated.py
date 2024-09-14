51302fa85d3238aa76030cae31a5c6bb63a4989f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-13 22:48:37 -0300

    começando problema

---
+++ harvard-cs50p/problems/problem_set_3/outdated.py
@@ -0,0 +1,21 @@
+#https://cs50.harvard.edu/python/2022/psets/3/outdated/
+
+def main():
+    ...
+
+months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+]
+
+main()
