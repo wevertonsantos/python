1b578ce73f1fa27bf794b706ed0f9d1db228ccdb
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-13 23:09:02 -0300

    desenrolando problema

--- harvard-cs50p/problems/problem_set_3/outdated.py
+++ harvard-cs50p/problems/problem_set_3/outdated.py
@@ -1,6 +1,12 @@
 #https://cs50.harvard.edu/python/2022/psets/3/outdated/
 
 def main():
+    date = input("Date: ")
+    formatter_date(date)
+
+def formatter_date(date):
+    #Verificar os dados que estão contido na data
+    #Pegar o dia, mês e ano da data
     ...
 
 months = [
@@ -18,4 +24,20 @@ months = [
     "December"
 ]
 
-main()
+#main()
+
+date = "September 8, 1636"
+if '/' in date and len(date) == 8:
+    month = month = int(date[0])
+    year = int(date[4:])
+    day = int(date[2])
+    if month < 10:
+        month = f"0{int(date[0])}"
+    if day < 10:
+        day = f"0{int(date[2])}"
+    date = f"{year}-{month}-{day}"
+    print(date)
+else:
+    for month in months:
+        if month in date:
+            ...
