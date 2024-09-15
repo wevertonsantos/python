08afd92b95a1948d016f40a853f5f6e88d91997d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-15 20:30:34 -0300

    random.randint

--- harvard-cs50p/libraries/generate.py
+++ harvard-cs50p/libraries/generate.py
@@ -1,4 +1,8 @@
+#https://docs.python.org/3/library/random.html
 import random #importando módulo
 
-coin = random.choice(["heads","tails"])
+coin = random.choice(["heads","tails"]) #choices
 print(coin)
+
+number = random.randint(1,10) #random number between 1 and 10
+print(number)
