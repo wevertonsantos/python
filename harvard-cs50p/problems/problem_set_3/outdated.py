03559baed9099433b2a943df4e5b0a7a55ddb8ba
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-15 20:01:37 -0300

    i'd finished outdated problem

--- harvard-cs50p/problems/problem_set_3/outdated.py
+++ harvard-cs50p/problems/problem_set_3/outdated.py
@@ -1,38 +1,39 @@
 #https://cs50.harvard.edu/python/2022/psets/3/outdated/
 
 def main():
-    date = input("Date: ")
-    print(formatter_date(date))
+    print(formatter_date())
 
-def formatter_date(date):
-    if "/" in date and len(date) <= 10:
-        date = date.split("/")
-        month = int(date[0])
-        day = int(date[1])
-        year = int(date[2])
-        if day <= 31 and month <= 12:
-            if month < 10:
-                month = f"0{month}"
-            if day < 10:
-                day = f"0{day}"
-            date = f"{year}-{month}-{day}"
-            return date
-    else:
-        for month in months:
-            if month in date:
-                if date.startswith(month):
-                    if len(date) <= 17:
-                        date = date.split(" ")
-                        month = int(months[month])
-                        day = int(date[1].replace(",","")) 
-                        year = int(date[2])
-                        if day <= 31 and month <= 12:
-                            if month < 10:
-                                month = f"0{month}"
-                            if day < 10:
-                                day = f"0{day}"
-                            date = f"{year}-{month}-{day}"
-                            return date  
+def formatter_date():
+    while True:
+        date = input("Date: ")
+        if "/" in date and len(date) <= 10:
+            date = date.split("/")
+            month = int(date[0])
+            day = int(date[1])
+            year = int(date[2])
+            if day <= 31 and month <= 12:
+                if month < 10:
+                    month = f"0{month}"
+                if day < 10:
+                    day = f"0{day}"
+                date = f"{year}-{month}-{day}"
+                return date
+        else:
+            for month in months:
+                if month in date:
+                    if date.startswith(month):
+                        if len(date) <= 17:
+                            date = date.split(" ")
+                            month = int(months[month])
+                            day = int(date[1].replace(",","")) 
+                            year = int(date[2])
+                            if day <= 31 and month <= 12:
+                                if month < 10:
+                                    month = f"0{month}"
+                                if day < 10:
+                                    day = f"0{day}"
+                                date = f"{year}-{month}-{day}"
+                                return date  
 
 months = {
     "January":1,
