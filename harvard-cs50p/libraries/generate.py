61aa1c7a4e8622ad895d16ae5fe2aa29883ce66f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-16 20:03:40 -0300

    shuffle cards

--- harvard-cs50p/libraries/generate.py
+++ harvard-cs50p/libraries/generate.py
@@ -6,3 +6,8 @@ print(coin)
 
 number = random.randint(1,10) #random number between 1 and 10
 print(number)
+
+cards = ["jack","queen","king"]
+random.shuffle(cards)
+for card in cards:
+    print(card)
