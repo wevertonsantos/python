aaae7a2c377e494ecc19cc5e2a2772797f6cc8f8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-16 20:32:16 -0300

    sys.exit

--- harvard-cs50p/libraries/name.py
+++ harvard-cs50p/libraries/name.py
@@ -3,8 +3,8 @@
 import sys
 
 if len(sys.argv) < 2:
-    print("Too few arguments")
+    sys.exit("Too few arguments")
 elif len(sys.argv) > 2:
-    print("Too many arguments")
-else:
-    print("hello, my name is", sys.argv[0], sys.argv[1])
+    sys.exit("Too many arguments")
+
+print("hello, my name is", sys.argv[0], sys.argv[1])
