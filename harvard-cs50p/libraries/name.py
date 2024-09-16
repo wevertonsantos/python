9baa31e3f12b8daa0cd668638bda47db11015b1e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-16 20:30:01 -0300

    statement with sys.

--- harvard-cs50p/libraries/name.py
+++ harvard-cs50p/libraries/name.py
@@ -6,5 +6,5 @@ if len(sys.argv) < 2:
     print("Too few arguments")
 elif len(sys.argv) > 2:
     print("Too many arguments")
-
-print("hello, my name is", sys.argv[0], sys.argv[1])
+else:
+    print("hello, my name is", sys.argv[0], sys.argv[1])
