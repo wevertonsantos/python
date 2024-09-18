dfa0cd9a38807eb79c451f108525a2cec198f701
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-18 20:50:51 -0300

    itunes with json

--- harvard-cs50p/apis/itunes.py
+++ harvard-cs50p/apis/itunes.py
@@ -6,5 +6,10 @@ import sys
 if len(sys.argv) != 2:
     sys.exit()
 
-response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
-print(json.dumps(response.json(), indent=2)) #formatting string
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=50&term=" + sys.argv[1])
+
+#print(json.dumps(response.json(), indent=2)) #formatting string
+
+o = response.json()
+for result in o["results"]:
+    print(result["trackName"])
