80a72e40cf988a3eacf784fd36072678faca5d8d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-18 20:44:29 -0300

    formatting json

---
+++ harvard-cs50p/apis/itunes.py
@@ -0,0 +1,10 @@
+#docs.python.org/3/library/json.html
+import json
+import requests
+import sys
+
+if len(sys.argv) != 2:
+    sys.exit()
+
+response = requests.get("https://itunes.apple.com/search?entity=song&limit=1&term=" + sys.argv[1])
+print(json.dumps(response.json(), indent=2)) #formatting string
