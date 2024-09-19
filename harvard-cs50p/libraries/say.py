0408c00df6bdbbb226fbc30c1e02973260defb8f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-18 21:01:21 -0300

    creating module and __main__

--- harvard-cs50p/libraries/say.py
+++ harvard-cs50p/libraries/say.py
@@ -1,7 +1,9 @@
 #https://pypi.org/project/cowsay/
 import cowsay
 import sys
+from sayings import hello #import module
 
 if len(sys.argv) == 2:
     #cowsay.cow("hello, " + sys.argv[1])
-    cowsay.trex("hello, " + sys.argv[1])
+    #cowsay.cow("hello, " + sys.argv[1])
+    hello(sys.argv[1])
