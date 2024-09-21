1c0b61eff350dcd64acc3ef6a1c4f51f152c25f2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-20 21:55:52 -0300

    weights with choices

--- harvard-cs50p/libraries/cards.py
+++ harvard-cs50p/libraries/cards.py
@@ -4,7 +4,7 @@ cards = ["jack","queen","king"]
 
 def main():
     print(random.choice(cards))
-    print(random.choices(cards,k=2))
+    print(random.choices(cards,weights=[75,20,0],k=2))
     print(random.sample(cards,k=2))
 
 main()
