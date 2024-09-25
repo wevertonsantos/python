bce73c9193639bd85f9d7b134e62ec8314cb7aa1
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-25 20:28:07 -0300

    melhorando o código.

--- harvard-cs50p/problems/problem_set_4/adieu.py
+++ harvard-cs50p/problems/problem_set_4/adieu.py
@@ -1,4 +1,6 @@
 #https://cs50.harvard.edu/python/2022/psets/4/adieu/
+import inflect
+p = inflect.engine()
 
 def main():
     list_names = []
@@ -7,18 +9,7 @@ def main():
             name = input("Name: ").strip()
             list_names.append(name)
     except KeyboardInterrupt:
-        output = "Adieu, adieu, to "
-        last_name = list_names[-1]
-        first_name = list_names[0]
-        if len(list_names) == 1:
-            output += name
-        elif len(list_names) == 2:
-            output += f"{first_name} and {last_name}"
-        else:
-            for one_name in list_names:
-                if one_name != last_name:
-                    output += f"{one_name}, "
-            output += f"and {last_name}"
+        output = "Adieu, adieu, to " + p.join((list_names))
         print(output)
 
 main()
