bd8f1c26fdc5854a9e9aed0e25f628f2be2f2015
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-09-27 23:02:52 -0300

    lógica

--- harvard-cs50p/problems/problem_set_4/game.py
+++ harvard-cs50p/problems/problem_set_4/game.py
@@ -1,6 +1,19 @@
 #https://cs50.harvard.edu/python/2022/psets/4/game/
 import random
 
-level = input("Level: ").strip()
-level = int(level)
-random.randint(1,level)
+level = input("Level: ")
+guess = input("Guess: ")
+while True:
+    try:
+        level = int(level)
+        guess = int(guess)
+        random_number = random.randint(1,level)
+        if guess < random_number:
+            print("Too small!")
+        elif guess > random_number:
+            print("Too large!")
+        else:
+            print("Just right!")
+        break
+    except ValueError:
+        level = input("Level: ")
