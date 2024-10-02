96f0f0313c2993c40a99ec7944ce6bb31644c35a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-10-02 20:05:30 -0300

    atribuindo main para o código.

--- harvard-cs50p/problems/problem_set_4/game.py
+++ harvard-cs50p/problems/problem_set_4/game.py
@@ -1,26 +1,29 @@
 #https://cs50.harvard.edu/python/2022/psets/4/game/
 import random
 
-level = input("Level: ")
-guess = None
-while True:
-    if level.isnumeric():
-        random_number = random.randint(1,int(level))
-        if guess == None:
-            guess = input("Guess: ")
-        if guess.isnumeric():
-            while True:
-                if int(guess) < random_number:
-                    print("Too small!")
-                    guess = input("Guess: ")
-                elif int(guess) > random_number:
-                    print("Too large!")
-                    guess = input("Guess: ")
-                else:    
-                    print("Just right!")
-                    break
-            break
+def main ():
+    level = input("Level: ")
+    guess = None
+    while True:
+        if level.isnumeric():
+            random_number = random.randint(1,int(level))
+            if guess == None:
+                guess = input("Guess: ")
+            if guess.isnumeric():
+                while True:
+                    if int(guess) < random_number:
+                        print("Too small!")
+                        guess = input("Guess: ")
+                    elif int(guess) > random_number:
+                        print("Too large!")
+                        guess = input("Guess: ")
+                    else:    
+                        print("Just right!")
+                        break
+                break
+            else:
+                guess = input("Guess: ")
         else:
-            guess = input("Guess: ")
-    else:
-        level = input("Level: ")
+            level = input("Level: ")
+
+main()
