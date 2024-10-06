a013289c33cd4b778cb2a66664bb1c94b31db312
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-10-05 22:17:13 -0300

    finalizando problema...

--- harvard-cs50p/problems/problem_set_4/professor.py
+++ harvard-cs50p/problems/problem_set_4/professor.py
@@ -6,38 +6,47 @@ import random
 problems = []
 while True:
     level = input("Level: ")
-    if int(level) == 1 or int(level) == 2 or int(level) == 3:
-        for i in range(10):
-            x = random.randint(1,int(10))
-            y = random.randint(1,int(10))
-            problems.append([x,y])
-        break
+    if level.isnumeric():
+        if int(level) == 1 or int(level) == 2 or int(level) == 3:
+            for i in range(10):
+                x = random.randint(1,int(10))
+                y = random.randint(1,int(10))
+                problems.append([x,y])
+            break
 
 times = 0
 score = 0
+error = False
 for problem in problems:
     x = problem[0]
     y = problem[1]
     result = x + y
-    #answer = int(input(f"{x} + {y} = "))
     while True:
-        answer = int(input(f"{x} + {y} = "))
-        if answer != result:
-            print("EEE")
-            times += 1
-            if times == 1:
+        try:
+            if error == False:
                 answer = int(input(f"{x} + {y} = "))
-                if answer == result:
-                    score += 1
-                    times = 0
-                    break 
-                else:
-                    print("EEE")
             else:
-                print(f"{x} + {y} = {result}")
+                error = False
+            if int(answer) != result:
+                print("EEE")
+                times += 1
+                if times == 1:
+                    answer = int(input(f"{x} + {y} = "))
+                    if answer == result:
+                        score += 1
+                        times = 0
+                        break 
+                    else:
+                        print("EEE")
+                else:
+                    print(f"{x} + {y} = {result}")
+                    times = 0
+                    break
+            else:
+                score += 1
                 times = 0
                 break
-        else:
-            score += 1
-            break
+        except ValueError:
+            answer = input(f"{x} + {y} = ")
+            error = True
 print(f"Score: {score}")
