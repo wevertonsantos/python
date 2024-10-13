27f0eb18a90e18ec82b4873eaa484d5a7851ffa9
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-10-13 20:07:24 -0300

    refatorando código

--- harvard-cs50p/problems/problem_set_4/bitcoin.py
+++ harvard-cs50p/problems/problem_set_4/bitcoin.py
@@ -3,27 +3,27 @@ import sys
 import requests
 
 def main():
-    response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
-    o = response.json()
+    try:
+        response = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+        o = response.json()
+    except requests.RequestException:
+        ...
+    bitcoins = check_argv()
+    bitcoins_to_usd(o,bitcoins)
+
+def check_argv():
     if len(sys.argv) < 2:
         sys.exit("Missing command-line argument")
     else:
         try:
-            bitcoins = float(sys.argv[1])
+            return float(sys.argv[1])
         except ValueError:
             sys.exit("Command-line argument is not a number")
-    bitcoins_to_usd(o,bitcoins)
-
-def check_argv():
-    ...
 
 def bitcoins_to_usd(o,bitcoins):
     usd = o['bpi']['USD']['rate_float']
     converted = bitcoins * usd
     output = f"${converted:,}"
-    return output
+    return print(output)
 
-try:
-    ...
-except requests.RequestException:
-    ...
+main()
