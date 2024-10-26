111161f510c20cb14833e2466e1ec7cbff844f42
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-10-26 19:32:35 -0300

    testes de número no meio, pontuações e primeiro número não pode ser zero

--- harvard-cs50p/problems/problem_set_5/test_plates/test_plates.py
+++ harvard-cs50p/problems/problem_set_5/test_plates/test_plates.py
@@ -2,8 +2,29 @@
 
 from plates import is_valid
 
+def test_first_number():
+    assert is_valid("0SS555") == False
+
 def test_start_two_letters():
     assert is_valid("C5") == False
 
 def test_contain_maximum_six_characters():
-    assert is_valid("AAA222") == True
+    assert is_valid("AAA2222") == False
+
+def test_numbers_not_in_middle():
+    assert is_valid("BSD22A") == False
+
+def test_periods():
+    assert is_valid("ACA:23") == False
+
+def test_space():
+    assert is_valid("CSS 5A") == False
+
+def test_first_punctuation():
+    assert is_valid("CSS.5A") == False
+
+def test_second_punctuation():
+    assert is_valid("CSS!5A") == False
+
+def test_third_punctuation():
+    assert is_valid("CSS,5A") == False
