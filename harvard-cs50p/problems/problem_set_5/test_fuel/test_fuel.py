1872bf88fd1325d466a0119ad8759fcf58ff01bf
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-10-27 16:11:40 -0300

    fazendo testes certo e errados para ver perfomance

--- harvard-cs50p/problems/problem_set_5/test_fuel/test_fuel.py
+++ harvard-cs50p/problems/problem_set_5/test_fuel/test_fuel.py
@@ -1,1 +1,27 @@
 #https://cs50.harvard.edu/python/2022/psets/5/test_fuel/
+
+from fuel import convert,gauge
+
+def test_one_divided_four():
+    assert convert('1/4') == 25
+
+def test_one_divided_two():
+    assert convert('1/2') == 50
+
+def test_x_y_integer():
+    assert convert('y/x') == ValueError
+
+def test_x_y_float():
+    assert convert('0.1/2') == ValueError
+
+def test_x_greater_than_y():
+    assert convert('100/2') == ValueError
+
+def test_y_is_zero():
+    assert convert('1/0') == ZeroDivisionError
+
+def test_return_e():
+    assert convert('0/1') == "E"
+
+def test_return_f():
+    assert convert('1/1') == "F"
