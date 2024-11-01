dbb64e2c48bc0992192b2a15c857c681c4e2fe76
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-11-01 19:42:32 -0300

    adicionando nomes de uma forma sem precisar de for each.

--- harvard-cs50p/file-io/names.py
+++ harvard-cs50p/file-io/names.py
@@ -1,12 +1,5 @@
-names = []
-for _ in range(3):
-    names.append(input("What's your name? "))
-print(names)
+name = input("What's your name? ")
 
-file = open("names.txt","w")
-for name in names:
-    if name == names[-1]:
-        file.write(f"{name}")
-    else:
-        file.write(f"{name}\n")
+file = open("names.txt","a")
+file.write(f"{name}")
 file.close
