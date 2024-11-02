d5d0472fff59d1e8ad7234d461e4b47847991eca
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-11-02 19:52:56 -0300

    trabalhando com with e readlines()

--- harvard-cs50p/file-io/names.py
+++ harvard-cs50p/file-io/names.py
@@ -1,5 +1,13 @@
+'''
 name = input("What's your name? ")
 
 file = open("names.txt","a")
 file.write(f"{name}")
 file.close
+'''
+
+with open("names.txt","r") as file: #working "with" with file
+    lines = file.readlines() #reading lines
+
+for line in lines:
+    print(f"Hello, {line.rstrip()}")
