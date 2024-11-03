96da129ba5e71fa58160929e6c2a14a000c40434
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-11-03 20:40:29 -0300

    trabalhando com arquivo csv + dicionário para segmentação de dados.

--- harvard-cs50p/file-io/students.py
+++ harvard-cs50p/file-io/students.py
@@ -1,4 +1,17 @@
+'''
 with open("students.csv") as file:
     for line in file:
         name, house = line.rstrip().split(",")
         print(f"{name} is in {house}")
+'''
+
+students = []
+
+with open("students.cs") as file:
+    for line in file:
+        name, house = line.rstrip().split(",")
+        student = {"name":name,"house":house}
+        students.append(student)
+
+for student in students:
+    print(f"{student['name']} is in {student['house']}")
