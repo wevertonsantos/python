deae04567be79f27785b2c0db90b06ae3347f659
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-11-09 18:59:00 -0300

    biblioteca csv.reader

--- harvard-cs50p/file-io/students.py
+++ harvard-cs50p/file-io/students.py
@@ -1,3 +1,5 @@
+import csv
+
 '''
 with open("students.csv") as file:
     for line in file:
@@ -7,14 +9,19 @@ with open("students.csv") as file:
 
 students = []
 
-with open("students.cs") as file:
+with open("students.csv") as file:
+    reader = csv.reader(file)
+    for name, home in reader:
+        students.append({"name": name,"home":home})
+    '''
     for line in file:
         name, house = line.rstrip().split(",")
         student = {"name":name,"house":house}
         students.append(student)
+    '''
 
 #def get_name(student):
 #    return student['name']
 
 for student in sorted(students,key=lambda student: student["name"]): #passing function to sorted by name
-    print(f"{student['name']} is in {student['house']}")
+    print(f"{student['name']} is from {student['home']}")
