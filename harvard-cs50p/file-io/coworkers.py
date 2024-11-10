ed5c9ed2a8ff1ee8b61ab67d234ccfe76893b187
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-11-10 19:43:51 -0300

    usando dictwriter, adicionando os campos que irão ser usados.

--- harvard-cs50p/file-io/coworkers.py
+++ harvard-cs50p/file-io/coworkers.py
@@ -4,5 +4,7 @@ name = input("What's your name? ")
 home = input("What's your home? ")
 
 with open("coworkers.csv","a") as file:
-    writer = csv.writer(file)
-    writer.writerow([name,home])
+    #writer = csv.writer(file)
+    #writer.writerow([name,home])
+    writer = csv.DictWriter(file,fieldnames=["name","home"])
+    writer.writerow({"name":name,"home":home})
