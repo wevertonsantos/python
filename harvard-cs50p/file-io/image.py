faf33b64b8bfcbe27abcf36f0330e58794c397e0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-03 20:33:56 -0300

    atribuindo filtro com pillow

--- harvard-cs50p/file-io/image.py
+++ harvard-cs50p/file-io/image.py
@@ -1,10 +1,12 @@
 from PIL import Image
+from PIL import ImageFilter
 
 def main():
     with Image.open('pilar.png') as img:
         #print(img.format)
         #print(img.size)
         img = img.rotate(100)
+        img = img.filter(ImageFilter.FIND_EDGES)
         img.save('out.png')
 
 main()
