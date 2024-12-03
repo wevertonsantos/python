eb6cfaaed0b6e1900e420de8a0dc2d7e850431c3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-03 20:31:14 -0300

    propriedade de alteração com pillow

--- harvard-cs50p/file-io/image.py
+++ harvard-cs50p/file-io/image.py
@@ -2,7 +2,9 @@ from PIL import Image
 
 def main():
     with Image.open('pilar.png') as img:
-        print(img.format)
-        print(img.size)
+        #print(img.format)
+        #print(img.size)
+        img = img.rotate(100)
+        img.save('out.png')
 
 main()
