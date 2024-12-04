6937a82ff43121ed122773b2fb8c706d644eb426
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-04 20:06:52 -0300

    adicionando novo arquivo para ser escrito, adicionando os campos do atual arquivo e colocando nova coluna.

--- harvard-cs50p/file-io/views.py
+++ harvard-cs50p/file-io/views.py
@@ -2,8 +2,11 @@ import csv
 from PIL import Image
 
 def main():
-    with open("views.csv","r",encoding="utf-8") as file:
-        reader = csv.DictReader(file)
+    with open("views.csv","r",encoding="utf-8") as views, open("analysis.csv","w", encoding="utf-8") as analysis:
+        reader = csv.DictReader(views)
+        writer = csv.DictWriter(analysis,fieldnames=reader.fieldnames + ["new"])
+        writer.writeheader()
+        new = "new_value"
         for row in reader:
             print(row["id"])
 
