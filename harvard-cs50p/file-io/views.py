9409f840b4060275eacdf247e2849aea83a4128b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-04 19:52:33 -0300

    abrindo csv e passando por linhas com DictReader

--- harvard-cs50p/file-io/views.py
+++ harvard-cs50p/file-io/views.py
@@ -2,6 +2,9 @@ import csv
 from PIL import Image
 
 def main():
-    ...
+    with open("views.csv","r",encoding="utf-8") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            print(row)
 
 main()
