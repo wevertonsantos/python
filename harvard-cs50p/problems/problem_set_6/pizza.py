31832fc0f1abfe029d883d464e72577831426f35
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-11 11:27:45 -0300

    if's in-line

--- harvard-cs50p/problems/problem_set_6/pizza.py
+++ harvard-cs50p/problems/problem_set_6/pizza.py
@@ -4,12 +4,9 @@ import csv
 from tabulate import tabulate
 
 def main():
-    if len(sys.argv) == 1:
-        sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2:
-        sys.exit("Too many command-line arguments")
-    elif '.csv' not in sys.argv[1]:
-        sys.exit("Not a CSV file")
+    if len(sys.argv) == 1: sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2: sys.exit("Too many command-line arguments")
+    elif '.csv' not in sys.argv[1]: sys.exit("Not a CSV file")
     else:
         try:
             with open(sys.argv[1]) as file:
@@ -22,16 +19,12 @@ def main():
 
 def append_rows(row,list):
     ''' Adding rows within list '''
-    if sys.argv[1] == "sicilian.csv":
-        list.append([row["Sicilian Pizza"], row["Small"], row["Large"]])
-    elif sys.argv[1] == "regular.csv":
-        list.append([row["Regular Pizza"], row["Small"], row["Large"]])
+    if sys.argv[1] == "sicilian.csv": list.append([row["Sicilian Pizza"], row["Small"], row["Large"]])
+    elif sys.argv[1] == "regular.csv": list.append([row["Regular Pizza"], row["Small"], row["Large"]])
 
 def grid(argv,rows):
     ''' Returning table as grid '''
-    if argv == "sicilian.csv":
-        return tabulate(rows,["Sicilian Pizza","Small","Large"],"grid")
-    elif argv == "regular.csv":
-        return tabulate(rows,["Regular Pizza","Small","Large"],"grid")
+    if argv == "sicilian.csv": return tabulate(rows,["Sicilian Pizza","Small","Large"],"grid")
+    elif argv == "regular.csv": return tabulate(rows,["Regular Pizza","Small","Large"],"grid")
 
 main()
