9279790f0c05eda76f8868809c4a3ce1eacf3cd4
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-11 17:13:48 -0300

    ajustando verificação if para 2 argumentos, escrevendo no arquivo com header passado no fieldnames. Adicionando as linhas no dicionário com as linhas do antigo arquivo.

--- harvard-cs50p/problems/problem_set_6/scourgify.py
+++ harvard-cs50p/problems/problem_set_6/scourgify.py
@@ -1,20 +1,22 @@
 #https://cs50.harvard.edu/python/2022/psets/6/scourgify/
-
 import csv
 import sys
 
 def main():
-    if len(sys.argv) == 1: sys.exit("Too few command-line arguments")
+    if len(sys.argv) < 3: sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3: sys.exit("Too many command-line arguments")
     elif '.csv' not in sys.argv[1]: sys.exit("Not a CSV File")
     else:
         try:
             with open(sys.argv[1],"r") as input, open(sys.argv[2],"w") as output:
                 reader = csv.DictReader(input)
-                writer = csv.DictReader(output,fieldnames=reader.fieldnames)
-                [print(line) for line in reader]
-                print(writer)
-                #writer = csv.DictWriter(output)
+                writer = csv.DictWriter(output,fieldnames=["first","last","house"])
+                writer.writeheader()
+                for row in reader:
+                    name = row["name"]
+                    first_last = name.split(",")
+                    house = row["house"]
+                    writer.writerow({"first":first_last[0],"last":first_last[1].strip(),"house":house})
         except FileNotFoundError:
             sys.exit(f"Could not read {sys.argv[1]}")
 
