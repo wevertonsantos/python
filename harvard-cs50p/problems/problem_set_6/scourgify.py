e2d59c759ea204a0cdbbd6365853c00555672f8d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-11 16:24:55 -0300

    fazendo verificação com 2 argumentos além do nome do arquivo python e fazendo tratamento de erro com os arquivos csv

--- harvard-cs50p/problems/problem_set_6/scourgify.py
+++ harvard-cs50p/problems/problem_set_6/scourgify.py
@@ -5,7 +5,7 @@ import sys
 
 def main():
     if len(sys.argv) == 1: sys.exit("Too few command-line arguments")
-    elif len(sys.argv) > 2: sys.exit("Too many command-line arguments")
+    elif len(sys.argv) > 3: sys.exit("Too many command-line arguments")
     elif '.csv' not in sys.argv[1]: sys.exit("Not a CSV File")
     else:
         try:
@@ -13,6 +13,6 @@ def main():
                 reader = csv.DictReader(input)
                 #writer = csv.DictWriter(output)
         except FileNotFoundError:
-            sys.exit("Could not read [sys.argv[1]]")
+            sys.exit(f"Could not read {sys.argv[1]}") if sys.argv[1] else sys.exit(f"Could not write {sys.argv[2]}")
 
 main()
