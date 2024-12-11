a1f3c528d0085ea2c2d6c973574d3c08a77ec768
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2024-12-11 16:12:37 -0300

    ajustando chamada de argumento na verificação do if, e abrindo arquivos com import csv.

--- harvard-cs50p/problems/problem_set_6/scourgify.py
+++ harvard-cs50p/problems/problem_set_6/scourgify.py
@@ -4,12 +4,14 @@ import csv
 import sys
 
 def main():
-    if len(sys.argv[1]) == 1: sys.exit("Too few command-line arguments")
-    elif len(sys.argv[1]) > 2: sys.exit("Too many command-line arguments")
+    if len(sys.argv) == 1: sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2: sys.exit("Too many command-line arguments")
     elif '.csv' not in sys.argv[1]: sys.exit("Not a CSV File")
     else:
         try:
-            ...
+            with open(sys.argv[1],"r") as input, open(sys.argv[2],"w") as output:
+                reader = csv.DictReader(input)
+                #writer = csv.DictWriter(output)
         except FileNotFoundError:
             sys.exit("Could not read [sys.argv[1]]")
 
