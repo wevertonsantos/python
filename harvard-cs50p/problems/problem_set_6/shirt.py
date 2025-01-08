a73ac19b87976c2d734301d0d3d2246c3687e358
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-08 16:35:44 -0300

    estruturando programa para resolver o problema.

---
+++ harvard-cs50p/problems/problem_set_6/shirt.py
@@ -0,0 +1,13 @@
+#https://cs50.harvard.edu/python/2022/psets/6/shirt/
+import sys
+
+def main():
+    verify_argv()
+
+def verify_argv():
+    if len(sys.argv) == 1:
+        ...
+    elif len(sys.argv) > 2:
+        ...
+
+main()
