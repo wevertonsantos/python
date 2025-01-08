16200440a8086fd88e063fedbe4402d37d037a7a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-08 16:45:29 -0300

    função com verificação de argumentos

--- harvard-cs50p/problems/problem_set_6/shirt.py
+++ harvard-cs50p/problems/problem_set_6/shirt.py
@@ -2,12 +2,12 @@
 import sys
 
 def main():
-    verify_argv()
+    if verify_arg():
+        print("ok")
 
-def verify_argv():
-    if len(sys.argv) == 1:
-        ...
-    elif len(sys.argv) > 2:
-        ...
+def verify_arg():
+    if len(sys.argv) == 1: sys.exit("Too few command-line arguments")
+    elif len(sys.argv) > 2: sys.exit("Too many command line-arguments")
+    else: return True
 
 main()
