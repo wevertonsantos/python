8593737ebabdbcfc89c12f37def7b04c86f859b4
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-09 16:57:07 -0300

    verificando se  nos argumentos tem apenas um "." e se o ponto está contido. Por fim, fazendo as validação se nos argumentos tem a mesma extensão e fazendo a verificação do index nos argumentos com validação se o input e o output tem a mesma extensão.

--- harvard-cs50p/problems/problem_set_6/shirt.py
+++ harvard-cs50p/problems/problem_set_6/shirt.py
@@ -4,15 +4,24 @@ from PIL import Image
 
 def main():
     if verify_arg():
-        with Image.open(sys.argv[1]) as input, open(sys.argv[2]) as output:
-            ...
+        #with Image.open(sys.argv[1]) as input, open(sys.argv[2]) as output:
+        ...
 
 def verify_arg():
+    index_first_arg = sys.argv[1].index(".") 
+    index_second_arg = sys.argv[2].index(".")
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3:
         sys.exit("Too many command line-arguments")
-    else:
-        return
+    elif "." in sys.argv[1] and sys.argv[1].count(".") == 1 and "." in sys.argv[2] and sys.argv[2].count(".") == 1:
+        if ".jpg" not in sys.argv[1] and ".jpeg" not in sys.argv[1] and ".png" not in sys.argv[1]:
+            sys.exit("Invalid input")
+        elif ".jpg" not in sys.argv[2] and ".jpeg" not in sys.argv[2] and ".png" not in sys.argv[2]:
+            sys.exit("Invalid input")
+    elif not sys.argv[1][index_first_arg:] == sys.argv[2][index_second_arg:]:
+        sys.exit("Input and output have different extensions")
+    else: 
+        return True
 
 main()
