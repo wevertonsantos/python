276a561dffcf2568d0684f5c2740a84df536fb52
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-11 08:58:50 -0300

    fazendo com que o argumento seja case-insensitively

--- harvard-cs50p/problems/problem_set_6/shirt.py
+++ harvard-cs50p/problems/problem_set_6/shirt.py
@@ -7,17 +7,17 @@ def main():
         #with Image.open(sys.argv[1]) as input, open(sys.argv[2]) as output:
         print("pass")
 
-def verify_arg(): 
+def verify_arg():
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3:
         sys.exit("Too many command line-arguments")
     elif sys.argv[1].count(".") != 1 or sys.argv[2].count(".") != 1:
         sys.exit("Invalid input")
-    elif "." in sys.argv[1] and sys.argv[1].count(".") == 1 and "." in sys.argv[2] and sys.argv[2].count(".") == 1:
-        if ".jpg" not in sys.argv[1] and ".jpeg" not in sys.argv[1] and ".png" not in sys.argv[1]:
+    elif "." in sys.argv[1].lower() and sys.argv[1].count(".") == 1 and "." in sys.argv[2].lower() and sys.argv[2].count(".") == 1:
+        if ".jpg" not in sys.argv[1].lower() and ".jpeg" not in sys.argv[1].lower() and ".png" not in sys.argv[1].lower():
             sys.exit("Invalid input")
-        elif ".jpg" not in sys.argv[2] and ".jpeg" not in sys.argv[2] and ".png" not in sys.argv[2]:
+        elif ".jpg" not in sys.argv[2].lower() and ".jpeg" not in sys.argv[2].lower() and ".png" not in sys.argv[2].lower():
             sys.exit("Invalid input")
         elif not sys.argv[1][sys.argv[1].index("."):] == sys.argv[2][sys.argv[2].index("."):]:  
             sys.exit("Input and output have different extensions")
