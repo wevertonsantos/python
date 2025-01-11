cb4d8bc84da677b41fe3ad4321627ddef491eaac
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-11 08:55:06 -0300

    verificando se o "."(ponto) está contido no argumento e verificando se as extensões são diferentes ou iguais.

--- harvard-cs50p/problems/problem_set_6/shirt.py
+++ harvard-cs50p/problems/problem_set_6/shirt.py
@@ -5,23 +5,23 @@ from PIL import Image
 def main():
     if verify_arg():
         #with Image.open(sys.argv[1]) as input, open(sys.argv[2]) as output:
-        ...
+        print("pass")
 
-def verify_arg():
-    index_first_arg = sys.argv[1].index(".") 
-    index_second_arg = sys.argv[2].index(".")
+def verify_arg(): 
     if len(sys.argv) < 3:
         sys.exit("Too few command-line arguments")
     elif len(sys.argv) > 3:
         sys.exit("Too many command line-arguments")
+    elif sys.argv[1].count(".") != 1 or sys.argv[2].count(".") != 1:
+        sys.exit("Invalid input")
     elif "." in sys.argv[1] and sys.argv[1].count(".") == 1 and "." in sys.argv[2] and sys.argv[2].count(".") == 1:
         if ".jpg" not in sys.argv[1] and ".jpeg" not in sys.argv[1] and ".png" not in sys.argv[1]:
             sys.exit("Invalid input")
         elif ".jpg" not in sys.argv[2] and ".jpeg" not in sys.argv[2] and ".png" not in sys.argv[2]:
             sys.exit("Invalid input")
-    elif not sys.argv[1][index_first_arg:] == sys.argv[2][index_second_arg:]:
-        sys.exit("Input and output have different extensions")
-    else: 
-        return True
+        elif not sys.argv[1][sys.argv[1].index("."):] == sys.argv[2][sys.argv[2].index("."):]:  
+            sys.exit("Input and output have different extensions")
+        else:
+            return True
 
 main()
