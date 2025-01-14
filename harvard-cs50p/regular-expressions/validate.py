6601344a5943c6852c2df19932fa3aeb793dd137
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-14 17:18:40 -0300

    verificando se domain termina com .edu

--- harvard-cs50p/regular-expressions/validate.py
+++ harvard-cs50p/regular-expressions/validate.py
@@ -2,7 +2,7 @@ email = input("What's your e-mail? ").strip()
 
 username, domain = email.split("@")
 
-if username and "." in domain:
+if username and domain.endswith(".edu"):
     print("Valid")
 else:
     print("Invalid")
