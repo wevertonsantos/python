24d49dd231d51809eb44286342c25b158d386520
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-20 10:21:10 -0300

    usado para verificar se dentro da string tem algo que atender ao padrão estabelecido.

--- harvard-cs50p/regular-expressions/regex.py
+++ harvard-cs50p/regular-expressions/regex.py
@@ -2,9 +2,11 @@ import re
 
 #compile - create pattern
 
-string = "python"
+string = "learning python"
 pattern = re.compile("python")
 
 result = re.fullmatch(pattern,string) #try to apply the pattern to all of the string
+
+result = re.search(pattern,string) #scan through string looking for a match to the pattern
 
 print(result)
