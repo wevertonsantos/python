c8ab919d37db115eb0d4caacdc6d87fa71b2b6e9
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-20 10:17:43 -0300

    fullmatch verifica se a string satisfaz inteiramente ao padrão criado.

--- harvard-cs50p/regular-expressions/regex.py
+++ harvard-cs50p/regular-expressions/regex.py
@@ -1,1 +1,10 @@
 import re
+
+#compile - create pattern
+
+string = "python"
+pattern = re.compile("python")
+
+result = re.fullmatch(pattern,string) #try to apply the pattern to all of the string
+
+print(result)
