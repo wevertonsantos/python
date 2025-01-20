8a046d2f996d8be7175428f3952fac478779d7ca
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-20 10:24:13 -0300

    pega todas as vezes na qual aquele conjunto de caracteres se encontra no padrão, retornando uma lista.

--- harvard-cs50p/regular-expressions/regex.py
+++ harvard-cs50p/regular-expressions/regex.py
@@ -2,11 +2,13 @@ import re
 
 #compile - create pattern
 
-string = "learning python"
+string = "learning python python"
 pattern = re.compile("python")
 
 result = re.fullmatch(pattern,string) #try to apply the pattern to all of the string
 
 result = re.search(pattern,string) #scan through string looking for a match to the pattern
+
+result = re.findall(pattern,string) #return a list of all non-overlapping matches in the string.
 
 print(result)
