2a8fc0932420154b2ec6b9456c81fb06c986e3a9
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-20 10:33:52 -0300

    "." representando caracter dentro da pesquisa.

--- harvard-cs50p/regular-expressions/regex.py
+++ harvard-cs50p/regular-expressions/regex.py
@@ -2,13 +2,25 @@ import re
 
 #compile - create pattern
 
-string = "learning python python"
+string = "learning python"
 pattern = re.compile("python")
 
-result = re.fullmatch(pattern,string) #try to apply the pattern to all of the string
+result = re.fullmatch(pattern, string) #try to apply the pattern to all of the string
 
 result = re.search(pattern,string) #scan through string looking for a match to the pattern
 
 result = re.findall(pattern,string) #return a list of all non-overlapping matches in the string.
+
+# "." representa qualquer caracter menos \n
+string = "python"
+pattern = re.compile("......")
+
+string = "py"
+pattern = re.compile("..")
+
+string = "python"
+pattern = re.compile("py....")
+
+result = re.fullmatch(pattern, string)
 
 print(result)
