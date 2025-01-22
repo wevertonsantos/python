5eb70ba3d4b5baadfa75ae39cf4254d5433f1a6a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-01-22 09:37:56 -0300

    verificação se é alfanumérico no regex.

--- harvard-cs50p/regular-expressions/regex.py
+++ harvard-cs50p/regular-expressions/regex.py
@@ -50,4 +50,10 @@ string = "a"
 pattern = re.compile("[^b]")
 result = re.fullmatch(pattern, string)
 
+# "\" - verificação se aquilo passado é um alfanumérico
+
+string = "@"
+pattern = re.compile("^[\w]")
+result = re.findall(pattern, string)
+
 print(result)
