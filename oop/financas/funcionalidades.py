665f0200890493f5edfd79dd21f27c37b829e0b5
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-01 16:58:05 -0300

    criando função de transação e de saldo total

--- oop/financas/funcionalidades.py
+++ oop/financas/funcionalidades.py
@@ -7,9 +7,18 @@ lista_transacoes = []
 def cadastrar_categoria(nome):
     nova_categoria = Categoria(nome=nome)
     lista_categorias.append(nova_categoria)
+    return nova_categoria
 
-def cadastrar_transacao():
-    nova_transacao = Transacao()
+def cadastrar_transacao(descricao,valor,categoria):
+    nova_transacao = Transacao(
+        descricao=descricao,
+        valor=valor,
+        categoria=categoria
+        )
+    lista_transacoes.append(nova_transacao)
+    return nova_transacao
 
 def saldo_total():
-    ...
+    total = 0
+    for transacao in lista_transacoes:
+        print(transacao.valor)
