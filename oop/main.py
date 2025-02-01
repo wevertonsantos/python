981dbf852c26f09fe5a493055bed648a73311979
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-01 15:10:06 -0300

    objeto e construtor.

--- oop/main.py
+++ oop/main.py
@@ -1,8 +1,11 @@
 class Cliente:
-    def __init__(self,nome,email,idade):
+    def __init__(self,nome,email): #construtor
         self.nome = nome
         self.email = email
-        self.idade = idade
 
     def exibir(self):
         print(self.email, self.idade)
+
+cliente_1 = Cliente("Guilherme","email@email.com") #objeto
+
+print(cliente_1)
