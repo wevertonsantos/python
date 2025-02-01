2a0658b875bbabc5b1b0bf04d51fd7fbf014e6fb
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-01 15:46:02 -0300

    criando metódo exibir para exibir nossa transação e os dados.

--- oop/financas/transacao.py
+++ oop/financas/transacao.py
@@ -6,3 +6,6 @@ class Transacao:
     descricao: str
     valor: float
     categoria: Categoria
+
+    def exibir(self):
+        print(f"Descrição: {self.descricao}, Valor: {self.valor}, Categoria: {self.categoria.nome}")
