e38c06a12b419b9af3e8cb2d3506f7a633daa003
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-01 17:21:33 -0300

    finalizando projeto mostrando saldo total.

--- oop/financas/main.py
+++ oop/financas/main.py
@@ -1,12 +1,33 @@
-from categoria import Categoria
-from transacao import Transacao
-
-c = Categoria(nome="Receitas")
-
-t1 = Transacao(
-    descricao="Salário Jan/2025",
-    valor=1000,
-    categoria=c
+from funcionalidades import (
+    cadastrar_categoria,
+    cadastrar_transacao,
+    saldo_total
 )
 
-t1.exibir()
+#Cadastrando categorias
+categoria_receitas = cadastrar_categoria("Receitas")
+categoria_contas = cadastrar_categoria("Contas Fixas")
+categoria_viagens = cadastrar_categoria("Viagens")
+
+#Cadastrando transações
+cadastrar_transacao(
+    descricao="Salário Jan/2025",
+    valor=1000.0,
+    categoria=categoria_receitas
+)
+
+cadastrar_transacao(
+    descricao="Conta de luz",
+    valor=-150.0,
+    categoria=categoria_contas
+)
+
+cadastrar_transacao(
+    descricao="Recife/2025",
+    valor=-1200.0,
+    categoria=categoria_viagens
+)
+
+total = saldo_total()
+
+print(f"Saldo total: {total}")
