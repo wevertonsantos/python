079096accf79b93a8743c4b67ff769ce8a29db66
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-02 13:01:09 -0300

    finalizando solução e mandando para leetcode.

--- leetcode/top-interview-150/merge_sorted_array.py
+++ leetcode/top-interview-150/merge_sorted_array.py
@@ -1,17 +1,15 @@
 #https://leetcode.com/problems/merge-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 
-nums1 = [-1,0,0,3,3,3,0,0,0]
-m = 6
-nums2 = [1,2,2]
+nums1 = [1,2,3,0,0,0]
+m = 3
+nums2 = [2,5,6]
 n = 3
 
-nums1.sort()
-for num1 in nums1[0:m]:
+for num1 in nums1[m:]:
     if num1 == 0:
-        nums1.remove(0)
-
-for num2 in nums2[0:n]:
+        nums1.remove(num1)
+        
+for num2 in nums2[:n]:
     nums1.append(num2)
 
-nums1.sort()
 print(nums1)
