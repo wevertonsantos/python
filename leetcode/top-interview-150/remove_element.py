5cc63495998f748683f14ce3b1584d98abbb56b0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-02 13:36:34 -0300

    atualizando código.

--- leetcode/top-interview-150/remove_element.py
+++ leetcode/top-interview-150/remove_element.py
@@ -1,13 +1,11 @@
 #https://leetcode.com/problems/remove-element/description/?envType=study-plan-v2&envId=top-interview-150
 
 nums = [3,2,2,3]
-val = 3
-
+val = 2
 k = 0
-for i in range(len(nums)):
+
+for num in nums:
     if val in nums:
-        if nums[i] == val:
-            k += 1
-            nums.remove(val)
-print(f"k = {k}")
+        nums.remove(val)
+print(f"k = {len(nums)}")
 print(nums)
