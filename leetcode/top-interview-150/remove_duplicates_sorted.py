72e077fd88075a80b55d025ced2196b58a0948cd
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-04 17:27:55 -0300

    finalizando problema;

--- leetcode/top-interview-150/remove_duplicates_sorted.py
+++ leetcode/top-interview-150/remove_duplicates_sorted.py
@@ -1,12 +1,8 @@
 #https://leetcode.com/problems/remove-duplicates-from-sorted-array/description/?envType=study-plan-v2&envId=top-interview-150
 
-nums = [1,1,1,1]
+nums = [0,0,1,1,1,2,2,3,3,4]
 
-for num in nums:
-    n = nums.count(num)
-    if n > 1:
-        nums.remove(1)
-        n -= 1
+for num in nums[:]:
+    if nums.count(num) > 1:
+        nums.remove(num)
 print(nums)
-
-# preciso remover números duplicados de um array, até ficar apenas 1
