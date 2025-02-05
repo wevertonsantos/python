386cc2792b76dfee8a3ae16a80ccb4633a8ca63e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-05 07:45:32 -0300

    em metade da solução do problema.

--- leetcode/top-interview-150/majority_element.py
+++ leetcode/top-interview-150/majority_element.py
@@ -1,3 +1,6 @@
 #https://leetcode.com/problems/majority-element/?envType=study-plan-v2&envId=top-interview-150
 
-nums = [3,2,3]
+nums =  [2,2,1,1,1,2,2]
+for num in nums:
+    if nums.count(num) > len(nums) / 2:
+        print(num)
