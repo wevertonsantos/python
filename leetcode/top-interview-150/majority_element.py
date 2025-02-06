0b78609eeb6e55495b87017d3b755029f56c0a7f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-06 09:04:14 -0300

    tentando melhorar tempo de execução.

--- leetcode/top-interview-150/majority_element.py
+++ leetcode/top-interview-150/majority_element.py
@@ -3,4 +3,10 @@
 nums =  [2,2,1,1,1,2,2]
 for num in nums:
     if nums.count(num) > len(nums) / 2:
-        print(num)
+        ...
+
+i = 0
+while i < sum(nums):
+    if nums.count(nums[i]) > len(nums) / 2:
+        print(nums[i])
+    i += 1
