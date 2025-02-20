da8714f4cb5f16543ffe255e48edd1519024b943
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-20 19:11:56 -0300

    realizando exercício de print.

---
+++ formacao-python-udemy/fundamentos/exercicios/print.py
@@ -0,0 +1,13 @@
+#1 - Realize o print do seu nome completo, sua idade e sua 
+# altura utilizando um print para cada valor.
+print("Weverton Santos")
+print(22)
+print(1.80)
+
+#2 - Realize o print do seu nome completo, sua idade e sua altura 
+# utilizando apenas um print para todos valores.
+print("Nome: {}, idade: {}, altura: {}".format("Weverton Santos",22,1.80))
+
+#3 - Realize o print de 3 números de sua escolha em um mesmo print, 
+# mas separados pelo caracter '-'.
+print(3,22,1, sep="-")
