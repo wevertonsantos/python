e9f626ed47740558d1329b0676c5da9eebeac4eb
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-21 08:31:03 -0300

    formatando número com apenas duas casas decimais.

--- formacao-python-udemy/fundamentos/formatacao.py
+++ formacao-python-udemy/fundamentos/formatacao.py
@@ -13,5 +13,9 @@ print(texto_formatado)
 nome = "Rodrigo"
 idade = 23
 altura = 1.73
-texto = "Meu nome é %s. Tenho %d anos e tenho %f m de altura" % (nome,idade,altura)
+texto = "Meu nome é %s. Tenho %d anos e tenho %.2f m de altura" % (nome,idade,altura)
 print(texto)
+
+# Formatando ponto flutuante
+numero_gigante = 1.123456789
+print("Número gigante formatado: %.2f" % numero_gigante)
