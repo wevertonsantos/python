dc9dbaaf3dc1fedcb2c580bce4196e5e9fa14aee
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-21 08:26:14 -0300

    formatação para texto

---
+++ formacao-python-udemy/fundamentos/formatacao.py
@@ -0,0 +1,11 @@
+# Formatação de texto
+
+# %s para texto
+nome = "Carolina"
+texto_formatado = "O nome dela é %s" % (nome)
+print(texto_formatado)
+
+# %d para inteiro
+
+
+# %f para ponto flutuante
