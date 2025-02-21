fbb372900e3286a05045ad2bdb8cb96e4d6f75de
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-21 08:19:15 -0300

    resolvendo problemas de variáveis.

---
+++ formacao-python-udemy/fundamentos/exercicios/variaveis.py
@@ -0,0 +1,19 @@
+#1 - Crie uma variável de cada tipo e ponha algum valor escolhido. 
+# Em seguida, printe todos esses valores.
+
+texto = "Olá"
+inteiro = 20
+flutuante = 20.5
+booleano = True
+
+print("Tipo texto: {}, tipo inteiro: {}, tipo flutuante {}, tipo booleano: {}".format(texto,inteiro,flutuante,booleano))
+
+#2 - Crie variáveis para guardar seu nome, CPF e uma que indique 
+# se você esta casado, em seguida printe esses valores separadamente, 
+# mas não esqueça de printar junto o que eles significam.
+
+nome = "Weverton Santos"
+cpf = "000.000.000-00"
+estado_civil = "Solteiro"
+
+print("Nome: {}, CPF: {}, estado civil: {}".format(nome,cpf,estado_civil))
