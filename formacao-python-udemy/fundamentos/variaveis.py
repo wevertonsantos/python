88ce28b355ff657454bf44abf43a6b4c55649ee7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-21 08:04:30 -0300

    tipos primitivos: variável sem nada com none

--- formacao-python-udemy/fundamentos/variaveis.py
+++ formacao-python-udemy/fundamentos/variaveis.py
@@ -1,5 +1,4 @@
-# Declarando variável
-
+# Declarando variável como numero
 numero = 1
 numero123 = 2
 print(numero,numero123)
@@ -7,3 +6,8 @@ print(numero,numero123)
 # Variável como string
 texto = "Olá mundo"
 print(texto)
+
+# Tipos primitivos
+
+variavel = None # variável sem nada
+print(variavel)
