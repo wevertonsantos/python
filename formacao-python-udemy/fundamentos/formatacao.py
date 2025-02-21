64c29f2a0fbe341c773afffaa3e9323c06079261
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-21 08:29:15 -0300

    formatação de texto com texto, fluatuante e inteiro.

--- formacao-python-udemy/fundamentos/formatacao.py
+++ formacao-python-udemy/fundamentos/formatacao.py
@@ -1,11 +1,17 @@
 # Formatação de texto
 
-# %s para texto
+'''
+%s para texto
+%d para inteiro
+%f para ponto flutuante
+'''
+
 nome = "Carolina"
 texto_formatado = "O nome dela é %s" % (nome)
 print(texto_formatado)
 
-# %d para inteiro
-
-
-# %f para ponto flutuante
+nome = "Rodrigo"
+idade = 23
+altura = 1.73
+texto = "Meu nome é %s. Tenho %d anos e tenho %f m de altura" % (nome,idade,altura)
+print(texto)
