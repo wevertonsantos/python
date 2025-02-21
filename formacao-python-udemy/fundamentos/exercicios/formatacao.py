0e10ba925b7f31671f4f819c53ad672b9f353cf1
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-21 08:51:32 -0300

    resolução dos exercícios de formatação.

---
+++ formacao-python-udemy/fundamentos/exercicios/formatacao.py
@@ -0,0 +1,20 @@
+# 1 - Escreva e formate a data em que você nasceu no formato dia/mês/ano. 
+# Não esqueça de criar 3 variáveis para guardar o dia, mês e ano.
+
+dia = 1
+mes = 11
+ano = 2002
+print("Eu nasci em {}/{}/{}".format(dia,mes,ano))
+
+# 2 - Escreva e formate a hora e minuto atual. 
+# Não esqueça de criar duas variáveis para guardar a hora e minuto.
+
+horas = 22
+minutos = 37
+print("Agora são {} horas e {} minutos".format(horas,minutos))
+
+# 3 - Escreva um programa que contêm o número PI, que deve ter o valor exato 
+# de 3.14159265359. Agora formate esse número para ter apenas cinco casas decimais.
+
+pi = 3.14159265359
+print("O PI é: %.5f" % (pi))
