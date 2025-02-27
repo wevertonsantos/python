6aa5fac2f39cb392c5ccf415b6d45f7c94544f0c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-27 10:51:04 -0300

    entendendo operadores lógicos.

---
+++ formacao-python-udemy/fundamentos/operadores_logicos.py
@@ -0,0 +1,10 @@
+'''
+and - resulta verdadeiro se todos os valores envolvidos na operação forem verdadeiros, caso contrário resulta false
+
+or - resulta em verdadeiro se qualquer um dos operadores for verdadeiro
+
+not - inverte o resultado lógico
+
+Not(True) = False
+Not(False) = True
+'''
