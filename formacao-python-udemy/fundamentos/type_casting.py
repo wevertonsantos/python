e34b260511e2b4c97c99b5f69707ad4f1180dd52
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-02-28 09:24:57 -0300

    exemplo de uso casting string

--- formacao-python-udemy/fundamentos/type_casting.py
+++ formacao-python-udemy/fundamentos/type_casting.py
@@ -24,3 +24,8 @@ texto = "O número é "
 numero = 10.3
 numero_string = str(numero) # convertendo para string
 print(texto, numero_string)
+
+numero = 123
+texto = str(numero)
+tamanho_texto = len(texto)
+print("O número %s tem %d digitos " % (texto,tamanho_texto))
