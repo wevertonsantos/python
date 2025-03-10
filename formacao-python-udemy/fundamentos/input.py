61ed8c956a26270f1d40b7eb0bd381c7a68fb4eb
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-03-10 16:40:51 -0300

    mostrando valor lógico através do input.

--- formacao-python-udemy/fundamentos/input.py
+++ formacao-python-udemy/fundamentos/input.py
@@ -5,7 +5,11 @@
 #nome = input()
 #print("Eu me chamo %s" % (nome))
 
-dia = input("Insira o dia: ")
-mes = input("Insira o mês: ")
-ano = input("Insira o ano: ")
-print("A data inserida foi %s/%s/%s" % (dia,mes,ano))
+#dia = input("Insira o dia: ")
+#mes = input("Insira o mês: ")
+#ano = input("Insira o ano: ")
+#print("A data inserida foi %s/%s/%s" % (dia,mes,ano))
+
+entrada = input("Digite 1 para verdadeiro e 0 para falso:")
+logico = bool(int(entrada))
+print("Valor lógico:",logico)
