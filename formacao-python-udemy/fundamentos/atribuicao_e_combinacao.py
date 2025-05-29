4234423b15d70534862740c38a0c4915879a418f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-05-29 16:19:10 -0300

    verificando se são iguais ou diferentes

--- formacao-python-udemy/fundamentos/atribuicao_e_combinacao.py
+++ formacao-python-udemy/fundamentos/atribuicao_e_combinacao.py
@@ -6,3 +6,12 @@ print(numero)
 numero = 10
 numero /= 2
 print(numero)
+
+num = 20
+booleana = (num == 10) #verificar se são iguais
+print(booleana)
+
+num = 20
+booleana = (num != 10) #verificação se são diferentes
+print(booleana)
+
