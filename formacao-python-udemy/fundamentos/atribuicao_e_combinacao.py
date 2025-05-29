d732e9e032e8f80ebf7e8fdfd6c3cd5e3098c581
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-05-29 16:22:51 -0300

    fazendo comparação maior ou menor.

--- formacao-python-udemy/fundamentos/atribuicao_e_combinacao.py
+++ formacao-python-udemy/fundamentos/atribuicao_e_combinacao.py
@@ -15,3 +15,13 @@ num = 20
 booleana = (num != 10) #verificação se são diferentes
 print(booleana)
 
+num1 = 10
+num2 = 20
+e_maior = num1 > num2
+print(e_maior)
+
+e_menor = num1 < num2
+print(e_menor)
+
+e_menor_ou_igual = num1 <= num2
+print(e_menor_ou_igual)
