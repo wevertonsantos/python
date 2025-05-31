b9f4244a224a69d98bfd813283c418e6ea559e6d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-05-31 18:15:17 -0300

    slicing mais strings

--- formacao-python-udemy/fundamentos/uso_strings.py
+++ formacao-python-udemy/fundamentos/uso_strings.py
@@ -12,3 +12,17 @@ print(texto1)
 texto = "Python é bem produtivo,"
 texto_repetido = texto * 3
 print(texto_repetido)
+
+#slicing strings
+texto = "exemplo"
+print(texto[0])
+print(texto[1])
+print(texto[1:3])
+print(texto[3:])
+print(texto[:5])
+
+texto = "carro"
+print(texto[-5]) #conta da direita para esquerda
+print(texto[-5:])
+print(texto[:-1])
+print(texto[-5:-2])
