59a7cbec32c147820cae69480c325e6410f5275b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-01 14:33:01 -0300

    primeiro exercício.

---
+++ formacao-python-udemy/fundamentos/exercicios/uso_strings.py
@@ -0,0 +1,6 @@
+# 1 - Crie um única string que contêm seu nome e sobrenome, em seguida use o slicing para separar o nome em uma variável e o seu sobrenome em outra. Printe esses valores.
+
+nome_completo = "Weverton Santos"
+nome = nome_completo[:8]
+sobrenome = nome_completo[9:]
+print(nome,sobrenome)
