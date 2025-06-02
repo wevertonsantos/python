19101b13cf6e18c44d7325acf9390d80cb9c2172
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-02 17:33:08 -0300

    usando elif aninhado com if

---
+++ formacao-python-udemy/estruturas_de_programacao/estrutura_if_elif_else.py
@@ -0,0 +1,9 @@
+numero = 11
+if numero < 10:
+    print("Menor que 10")
+elif numero < 100:
+    print("Menor que 100")
+elif numero <= 1000:
+    print("Menor ou igual a 1000")
+else:
+    print("Nenhuma anterior")
