25ff0d8576955300ce0a8140e6377c9e5d60e346
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-02 17:19:45 -0300

    verificação de par e impar com if else

--- formacao-python-udemy/estruturas_de_programacao/estrutura_if_else.py
+++ formacao-python-udemy/estruturas_de_programacao/estrutura_if_else.py
@@ -3,3 +3,9 @@ if (numero >= 10):
     print("É maior ou igual a 10")
 else: #se não, se a condição for falsa
     print("É menor que 10")
+
+numero = 10
+if numero % 2 == 0:
+    print("É par")
+else:
+    print("É impar")
