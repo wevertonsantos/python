b4a7cf786aad6d8f1447a5234ea06f3128d4a06c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-03 15:47:40 -0300

    verificação de par ou impar com while do 2 a 10

--- formacao-python-udemy/estruturas_de_programacao/estrutura_lacos_while.py
+++ formacao-python-udemy/estruturas_de_programacao/estrutura_lacos_while.py
@@ -6,3 +6,11 @@ while interacao > 0:
     print(interacao)
     interacao -= 1
 print("Encerrou")
+
+indice = 10
+while indice >= 2:
+    if indice % 2 == 0:
+        print("O número %d é par" % indice)
+    else:
+        print("O número %d é impar" % indice)
+    indice -= 1
