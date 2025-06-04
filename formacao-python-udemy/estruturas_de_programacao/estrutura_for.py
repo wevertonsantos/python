b469ad253f474412b6f9b6b697ce307ec008c8f3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-04 15:46:52 -0300

    achando letra no texto utilizando for.

--- formacao-python-udemy/estruturas_de_programacao/estrutura_for.py
+++ formacao-python-udemy/estruturas_de_programacao/estrutura_for.py
@@ -13,3 +13,9 @@ for x in range(20,5,-5):
 texto = "123456789"
 for x in range(0,len(texto)):
     print(texto[x])
+
+letra = input("Digite uma letra: ")
+texto = input("Digite um texto: ")
+for i in range(0,len(texto)):
+    if letra == texto[i]:
+        print("Encontrei a letra %s na posição %d" % (letra, i))
