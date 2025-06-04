e344b14920ac418b197e6e90da5e9a6f240025f5
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-04 17:06:37 -0300

    tentando resolver problema..

--- formacao-python-udemy/estruturas_de_programacao/exercicios/estrutura_for.py
+++ formacao-python-udemy/estruturas_de_programacao/exercicios/estrutura_for.py
@@ -6,3 +6,10 @@ for i in texto:
     if i != " ":
         caracter += 1
 print("Esse texto tem exatamente %d caracter(es)" % caracter)
+
+# 2 - Crie um programa que faça o calculo do fatorial de um número. O fatorial a ser calculado deve ser recebido por input.
+
+fatorial = int(input("Digite o fatorial a ser calculado: "))
+for i in range(1,fatorial):
+    print(fatorial)
+#    5 * 4 * 3 * 2 * 1
