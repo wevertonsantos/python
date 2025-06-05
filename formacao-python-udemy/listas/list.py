a0983cb711efc1ba962c08ad8c49d9d3d9ac2eba
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-05 15:14:10 -0300

    Método lista com números

--- formacao-python-udemy/listas/list.py
+++ formacao-python-udemy/listas/list.py
@@ -6,3 +6,5 @@ numeros = [1,2,3] # Lista com números
 floats = [5.3,-2.2,0.5] # Lista com pontos flutuantes
 strings = ["A","B","C"] # Lista com strings
 misto = [2,2.3,"ABC"] # Lista com dados diferentes
+
+array = list([1,2,3]) # Método lista com números
