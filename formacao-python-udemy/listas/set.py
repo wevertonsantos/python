34f379ca56ccc4f8bdbcd93f5d3578ab0444767d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-09 17:23:55 -0300

    Removendo valores

--- formacao-python-udemy/listas/set.py
+++ formacao-python-udemy/listas/set.py
@@ -4,4 +4,5 @@ set = {1,2,3}
 #print(set[0]) # Não podemos olhar determinada posição
 #set[0] = 1 # Não pode alterar valores
 set.add(5) # Adicionando elemento
+set.remove(1) # Removendo valores
 print(set)
