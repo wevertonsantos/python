7bb177b6829002892430446aa1577e8eaa79a426
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-09 17:22:57 -0300

    Adicionando elemento

--- formacao-python-udemy/listas/set.py
+++ formacao-python-udemy/listas/set.py
@@ -1,6 +1,7 @@
 # Declaração set:
 set = {}
 set = {1,2,3}
-print(set)
 #print(set[0]) # Não podemos olhar determinada posição
-set[0] = 1 # Não pode alterar valores
+#set[0] = 1 # Não pode alterar valores
+set.add(5) # Adicionando elemento
+print(set)
