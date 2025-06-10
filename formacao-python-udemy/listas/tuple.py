e50abf45a0c89c82fb395f42550e118a3846010a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-10 18:57:49 -0300

    Fazendo casting com tupla.

--- formacao-python-udemy/listas/tuple.py
+++ formacao-python-udemy/listas/tuple.py
@@ -1,9 +1,16 @@
-tuple = tuple(('chocolate','bom bom','paçoca'))
-tuple = ('chocolate','bom bom','paçoca')
-print(tuple)
+tupla = tuple(('chocolate','bom bom','paçoca'))
+tupla = ('chocolate','bom bom','paçoca')
+print(tupla)
 
 numeros = (1,1,2,3,4,5)
 print(numeros[2]) # Permitido acessar índice.
 print(numeros[1:3]) # Slices com tuple
 #numeros[0] = '10' # Não é permitido alteração
 print(numeros.count(1)) # Ocorrência desse elemento em tuple
+
+tupla = (1,2,3)
+lista = list(tupla)
+lista[0] = 12
+lista.append("Fim")
+tupla = tuple(lista)
+print(tupla)
