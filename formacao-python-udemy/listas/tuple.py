e4a62b8cfb62e82eaa6cfb26674844b4a4da7ee3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-10 18:49:26 -0300

    Ocorrência desse elemento em tuple

--- formacao-python-udemy/listas/tuple.py
+++ formacao-python-udemy/listas/tuple.py
@@ -2,7 +2,8 @@ tuple = tuple(('chocolate','bom bom','paçoca'))
 tuple = ('chocolate','bom bom','paçoca')
 print(tuple)
 
-numeros = ('1','2','3')
+numeros = (1,1,2,3,4,5)
 print(numeros[2]) # Permitido acessar índice.
 print(numeros[1:3]) # Slices com tuple
-numeros[0] = '10' # Não é permitido alteração
+#numeros[0] = '10' # Não é permitido alteração
+print(numeros.count(1)) # Ocorrência desse elemento em tuple
