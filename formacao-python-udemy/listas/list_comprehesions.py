662bca42b0c28cddf60f5098ba014a474bff8694
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-12 19:01:35 -0300

    Usando lista comprehension com if

--- formacao-python-udemy/listas/list_comprehesions.py
+++ formacao-python-udemy/listas/list_comprehesions.py
@@ -6,3 +6,7 @@ print(lista)
 lista = ['zero','um','três']
 lista = [x for x in lista]
 print(lista)
+
+# Usando lista comprehension com if
+lista = [x for x in range(0,10) if x % 2 == 0]
+print(lista)
