cffb4590c159a220e4fdc414603f7dffe095140f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-12 18:49:58 -0300

    Acessando dicionário dentro de um dicionário

--- formacao-python-udemy/listas/dictionary.py
+++ formacao-python-udemy/listas/dictionary.py
@@ -37,3 +37,24 @@ print(chaves)
 # Limpando dicionário
 idades.clear()
 print(idades)
+
+# Acessando dicionário dentro de um dicionário
+
+dados_maria = {
+    'sexo' : 'feminino',
+    'cpf' : '12345678910',
+    'rg' : '233232432'
+}
+
+dados_joao = {
+    'sexo' : 'masculino',
+    'cpf' : '12345678910',
+    'rg' : '233232233'
+}
+
+dados_nome = {
+    'maria' : dados_maria,
+    'joao' : dados_joao
+}
+
+print(dados_nome)
