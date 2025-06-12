384036eba76409bc765beaf3d8ae646b37b70d1b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-12 19:04:45 -0300

    Usando lista comprehension com mais de uma condição lógica

--- formacao-python-udemy/listas/list_comprehesions.py
+++ formacao-python-udemy/listas/list_comprehesions.py
@@ -10,3 +10,7 @@ print(lista)
 # Usando lista comprehension com if
 lista = [x for x in range(1,11) if x % 2 == 0]
 print(lista)
+
+# Usando lista comprehension com mais de uma condição lógica
+lista = [x for x in range(-10,11) if x < 10 if x >= 0]
+print(lista)
