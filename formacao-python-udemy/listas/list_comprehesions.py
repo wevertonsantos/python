3d80f6ff6050e477c4180b6220d2bb7138d52717
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-12 19:00:15 -0300

    Percorrendo lista

--- formacao-python-udemy/listas/list_comprehesions.py
+++ formacao-python-udemy/listas/list_comprehesions.py
@@ -1,3 +1,8 @@
 # Declaração de uma lista comprehension
 lista = [x for x in range(0,10)]
 print(lista)
+
+# Percorrendo lista
+lista = ['zero','um','três']
+lista = [x for x in lista]
+print(lista)
