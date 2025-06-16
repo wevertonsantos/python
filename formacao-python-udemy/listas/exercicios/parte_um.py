c375ab93499b3a4b9b9623332207ae1ee35bbbdf
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-16 17:02:19 -0300

    Ex. 1

---
+++ formacao-python-udemy/listas/exercicios/parte_um.py
@@ -0,0 +1,7 @@
+# 1 - Crie uma lista com os seguintes números 1,10,6,7,8,10. Em seguida printe a soma destes números.
+
+lista = [1,10,6,7,8,10]
+total = 0
+for num in lista:
+    total += num
+print("Soma dos números:",total)
