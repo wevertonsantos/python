a0db159cbc263480cb03a82fe87c1bb30b95728e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-16 16:53:50 -0300

    criando outra matriz.

--- formacao-python-udemy/listas/list_comprehesions.py
+++ formacao-python-udemy/listas/list_comprehesions.py
@@ -32,5 +32,4 @@ lista = [1,2,3,4,5,6,7,8,9]
 lista = list(filter(lambda x: x % 2 == 0, lista))
 print(lista)
 
-lista = [[x for x in range(1,5)] for y in range(1,5)]
-print(lista)
+lista = [[str(x) + str(y) for x in range(1,5)] for y in range(1,5)]
