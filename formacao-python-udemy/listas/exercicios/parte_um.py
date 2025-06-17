ccfd0b8e215ce53ad56abb426a50cab0f7563ad9
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-17 18:16:30 -0300

    Ex.5

--- formacao-python-udemy/listas/exercicios/parte_um.py
+++ formacao-python-udemy/listas/exercicios/parte_um.py
@@ -28,3 +28,12 @@ meses = {1:'janeiro',2:'fevereiro',3:'março',4:'abril',5:'maio',6:'junho',7:'ju
 
 numero_mes = int(input('Digite o número do mês que você nasceu: '))
 print('Você nasceu no mês:',meses.get(numero_mes))
+
+# 5 - Crie uma lista contendo todos dias (número) do mês em que você nasceu. Em seguida receba por input o dia (número) em que você nasceu e remova  desta lista. Ao final mostre o conteúdo da lista.
+
+dias  = []
+for i in range(1,32):
+  dias.append(i)
+dia_nascido = int(input("Digite o número do dia em que você nasceu: "))
+dias.remove(dia_nascido)
+print(dias)
