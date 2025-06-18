3acea97a8924ef7ca7ee1f3d33a0ad36b7c809a1
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-18 15:12:18 -0300

    Ex. 7

--- formacao-python-udemy/listas/exercicios/parte_um.py
+++ formacao-python-udemy/listas/exercicios/parte_um.py
@@ -51,3 +51,8 @@ if objeto in objetos:
   print('Função do objeto escolhido:',objetos.get(objeto))
 else:
   print("Objeto não encontrado")
+
+# 7 - Crie uma lista contendo todos os números negativos de -30 até -20. Printe essa lista.
+
+lista = [x for x in range(-30,-19)]
+print(lista)
