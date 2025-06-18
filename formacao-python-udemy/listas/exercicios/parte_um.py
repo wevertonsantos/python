59ba76ecbcf76cca3442129f235352ef9ec74587
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-18 15:36:06 -0300

    Ex. 10

--- formacao-python-udemy/listas/exercicios/parte_um.py
+++ formacao-python-udemy/listas/exercicios/parte_um.py
@@ -67,3 +67,8 @@ print(numeros)
 
 numeros = [x for x in range(0,101) if str(x)[-1] == '0']
 print(numeros)
+
+# 10 - Percorra os números de 0 a 20 e crie um array, onde caso o número terminar com zero o valor devera ser '0', caso contrario devera ser '-'.
+
+lista = ['0' if str(x)[-1] == '0' else '-' for x in range(0,21)] 
+print(lista)
