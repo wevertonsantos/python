6c5785359f1ed284aac895052b3280ee1d1544a5
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-18 15:21:58 -0300

    Ex. 9

--- formacao-python-udemy/listas/exercicios/parte_um.py
+++ formacao-python-udemy/listas/exercicios/parte_um.py
@@ -61,3 +61,9 @@ print(lista)
 
 numeros = [x for x in range(4,101) if x % 4 == 0]
 print(numeros)
+
+
+# 9 - Crie uma lista contendo todos os números de 0 a 100, mas filtre, mantendo apenas os números que terminam com 0.
+
+numeros = [x for x in range(0,101) if str(x)[-1] == '0']
+print(numeros)
