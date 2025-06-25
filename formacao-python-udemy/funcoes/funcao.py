ecc24f5e03648a9d91c3468062d576544c67e639
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-25 15:28:07 -0300

    Utilizando decorator

--- formacao-python-udemy/funcoes/funcao.py
+++ formacao-python-udemy/funcoes/funcao.py
@@ -167,3 +167,10 @@ def Maiusculo(func):
     def inner_func():
         return func().upper()
     return inner_func
+
+@Maiusculo # Utilizando decorator
+def retorna_string():
+    return "string de teste"
+
+valor = retorna_string()
+print(valor)
