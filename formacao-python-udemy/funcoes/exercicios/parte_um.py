3a32d769eaa08ce76fdcf040b89260bc71003cb0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-26 13:44:35 -0300

    Ex. 1

---
+++ formacao-python-udemy/funcoes/exercicios/parte_um.py
@@ -0,0 +1,11 @@
+'''
+Crie uma função chamada “e_negativo” que receba um número, retorna um booleano “True” se o número for negativo, caso contrário retorna “False”.
+'''
+
+def e_negativo(num):
+ if num < 0:
+  return True
+ else:
+  return False
+ 
+print(e_negativo(0))
