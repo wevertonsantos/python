f600192d488bab1d411248379ab5bbb0c592c547
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-28 11:03:54 -0300

    Traz o maior valor func: max()

--- formacao-python-udemy/builtin/funcoes_matematica.py
+++ formacao-python-udemy/builtin/funcoes_matematica.py
@@ -3,3 +3,6 @@ num2 = 10
 print(abs(num1))
 print(abs(num2))
 print(abs(num1) + abs(num2)) # Absoluto que desconsidera o sinal
+
+maior_valor = max(10,20,30) # Traz o maior valor
+print(maior_valor)
