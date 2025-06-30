e9d46a5b99f6db24adde830a7fa5ab26efd7f774
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-30 13:17:36 -0300

    Mudando valor do range

--- formacao-python-udemy/builtin/caracteres_numero.py
+++ formacao-python-udemy/builtin/caracteres_numero.py
@@ -2,6 +2,6 @@ numero = 70
 caractere = chr(numero) # Conversão de número para caractere
 print("O número %d é mapeado para o caractere '%s'" % (numero,caractere))
 
-for i in range(1,100):
+for i in range(32,100):
     caractere = chr(i)
     print("O número %d é mapeado para o caractere '%s'" % (i,caractere))
