00b9884c34abdeaf36736dcfd37795709c1225cf
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-30 13:24:08 -0300

    Transforma cada primeira letra de cada palavra em maiúsculo

--- formacao-python-udemy/builtin/funcoes_string.py
+++ formacao-python-udemy/builtin/funcoes_string.py
@@ -6,3 +6,6 @@ texto = "IsTo é EsTranHo"
 print(texto.lower()) # Texto tudo minúsculo
 print(texto.upper()) # Texto tudo maiúsculo
 print(texto.swapcase()) # Texto invertido
+
+texto = "eu sou um ótimo programador python"
+print(texto.title()) # Transforma cada primeira letra de cada palavra em maiúsculo
