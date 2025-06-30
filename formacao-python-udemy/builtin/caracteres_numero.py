84cedfe40ca0772bcfa2d20f5692d87104139a29
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-30 13:19:17 -0300

    Conversão de caractere para número

--- formacao-python-udemy/builtin/caracteres_numero.py
+++ formacao-python-udemy/builtin/caracteres_numero.py
@@ -5,3 +5,7 @@ print("O número %d é mapeado para o caractere '%s'" % (numero,caractere))
 for i in range(32,100):
     caractere = chr(i)
     print("O número %d é mapeado para o caractere '%s'" % (i,caractere))
+
+caractere = 'F'
+numero = ord(caractere) # Conversão de caractere para número
+print("O caractere %s é mapeado para o número %d" % (caractere,numero))
