53e5e0368dc10573b3940f6a0897e47b1c2b29d3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-06-30 13:23:07 -0300

    Alterando texto para minúsculo, maiúsculo e invertido.

---
+++ formacao-python-udemy/builtin/funcoes_string.py
@@ -0,0 +1,8 @@
+# Deixando primeira letra maiúscula
+texto = "olá, mundo"
+print(texto.capitalize())
+
+texto = "IsTo é EsTranHo"
+print(texto.lower()) # Texto tudo minúsculo
+print(texto.upper()) # Texto tudo maiúsculo
+print(texto.swapcase()) # Texto invertido
