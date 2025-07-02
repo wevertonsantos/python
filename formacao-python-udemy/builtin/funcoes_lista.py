528bf5c45ad40b58cb60cf8d7d30a4c8b8067041
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-02 13:03:17 -0300

    Criando função para odernar pelo tamanho das strings e fazendo o sort pelo tamanho.

--- formacao-python-udemy/builtin/funcoes_lista.py
+++ formacao-python-udemy/builtin/funcoes_lista.py
@@ -5,3 +5,10 @@ print(lista)
 lista = ["a","b","x","ab","d","c"]
 lista.sort(reverse=True) # Ordenando lista decrescente
 print(lista)
+
+def sort_por_tamanho(item): # Criando função para odernar pelo tamanho das strings
+    return len(item)
+
+lista = ["a","b","ab","de","abc","abcd"]
+lista.sort(key=sort_por_tamanho)
+print(lista)
