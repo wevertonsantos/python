c413ce17f82528b42d8d5208d363da760d4d53c7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 12:35:12 -0300

    Pegando apenas data e hora

--- formacao-python-udemy/builtin/funcoes_data_hora.py
+++ formacao-python-udemy/builtin/funcoes_data_hora.py
@@ -2,5 +2,5 @@ import datetime
 
 data_completa = datetime.datetime.now() # Pegando data e hora no agora
 
-data = data_completa.date()
-hora = data_completa.time()
+data = data_completa.date() # Pegando apenas data
+hora = data_completa.time() # Pegando apenas hora
