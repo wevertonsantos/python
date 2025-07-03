92e47d9b0c3058dc280ffb4e2e3bce12847f187b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 18:07:54 -0300

    Acrescentando semanas à data atual com timedelta.

--- formacao-python-udemy/builtin/funcoes_data_hora.py
+++ formacao-python-udemy/builtin/funcoes_data_hora.py
@@ -30,3 +30,6 @@ data1 = datetime.datetime(2020,10,20)
 data2 = datetime.datetime(2020,11,20)
 diferenca = data2 - data1
 print(diferenca) # Calculando diferença com data
+
+data_atual = datetime.now()
+datafutura1 = data_atual + timedelta(weeks=4) # acrescentando 4 semanas à data atual
