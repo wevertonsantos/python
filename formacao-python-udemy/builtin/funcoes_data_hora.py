1f2609c853a9451f6c3d7ab442fc7b5f22cce663
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-03 12:38:18 -0300

    Pegando data completa(dia,mes,ano,hora,minuto,segundo.)

--- formacao-python-udemy/builtin/funcoes_data_hora.py
+++ formacao-python-udemy/builtin/funcoes_data_hora.py
@@ -4,3 +4,10 @@ data_completa = datetime.datetime.now() # Pegando data e hora no agora
 
 data = data_completa.date() # Pegando apenas data
 hora = data_completa.time() # Pegando apenas hora
+
+print("Dia", data_completa.day)
+print("Mês", data_completa.month)
+print("Ano", data_completa.year)
+print("Hora", data_completa.hour)
+print("Minuto", data_completa.minute)
+print("Segundo", data_completa.second)
