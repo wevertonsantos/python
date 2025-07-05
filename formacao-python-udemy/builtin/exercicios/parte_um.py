b357127a98699d98df2d4d43c8cfd45e44862863
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-05 12:41:57 -0300

    ex. 4

--- formacao-python-udemy/builtin/exercicios/parte_um.py
+++ formacao-python-udemy/builtin/exercicios/parte_um.py
@@ -27,3 +27,17 @@ def menor(*args):
     return min(args)
 
 print(menor(960,203,50))
+
+"""
+4 - Crie uma função que calcule a formula de Bhaskara, encontrado o X. 
+Os coeficientes a, b e c devem ser lidos por input.
+"""
+
+import math
+a = float(input("Digite o coeficiente a:"))
+b = float(input("Digite o coeficiente b:"))
+c = float(input("Digite o coeficiente c:"))
+
+x1 = (-b + math.sqrt(pow(b,2) - 4*a*c))/ (2*a)
+x2 = (-b - math.sqrt(pow(b,2) - 4*a*c))/ (2*a)
+print("A solução encontrada é ", x1, x2)
