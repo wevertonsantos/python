289714804fc652376b051f9c10e998e28552023b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-05 12:31:47 -0300

    Ex. 1

---
+++ formacao-python-udemy/builtin/exercicios/parte_um.py
@@ -0,0 +1,6 @@
+# 1 - Crie uma função que retorne a subtração de dois elementos, mas que considere o valor absoluto deste valores.
+
+def subtracao(x,y):
+    return (abs(x) - abs(y))
+
+print(subtracao(-10,5))
