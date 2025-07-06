dcb32ca0c0ce4d45b4695567e5ac1e36465312e4
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-06 18:30:41 -0300

    ex. 7

--- formacao-python-udemy/builtin/exercicios/parte_dois.py
+++ formacao-python-udemy/builtin/exercicios/parte_dois.py
@@ -4,7 +4,23 @@ Retorne a quantidade de vezes que essa letra tem na string.
 Caso não ocorra nenhuma vez, retorne 0.
 '''
 
-def ocorrencia(string,letra):
+def ocorrencias(string,letra):
     return string.count(letra)
 
-print(ocorrencia("olamundo","o"))
+print(ocorrencias("olamundo","o"))
+
+'''
+7 - Crie uma função receba uma string e uma letra do alfabeto. 
+Retorne uma lista contendo o índice de onde todas as ocorrências aparecem.
+'''
+
+def ocorrencias(texto,caracter):
+  indices = []
+  indice =0
+  for char in texto:
+    if (char == caracter):
+      indices.append(indice)
+    indice += 1
+  return indices
+
+print(ocorrencias("abcaa","a"))
