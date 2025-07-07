9ae87898aeb0e709f2aefbac686091fa6dd1b731
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-07 13:29:06 -0300

    atributos estáticos e verificando número de pessoas a cada instância

--- formacao-python-udemy/oop/oop.py
+++ formacao-python-udemy/oop/oop.py
@@ -11,14 +11,18 @@ class NossaClasse: # criando classe
 var = NossaClasse()
 
 class Pessoa:
+    especie = "Homo Sapiens" # atributo estático
+    num_pessoas = 0 # outro atributo estático
     def __init__(self,nome,idade): # atribuindo parâmetros
-        especie = "Homo Sapiens" # atributo estático
         self.nome = nome # self é o próprio objeto que recebe nome
         self.idade = idade
         print("Pessoa com nome %s e idade %d criada" % (nome,idade))
+        Pessoa.num_pessoas += 1 # a cada instância atribuí 1
 
 pessoa1 = Pessoa('Rodrigo',25) # chamando instância do objeto
+print(Pessoa.num_pessoas)
 pessoa2 = Pessoa('Lucas',21)
+
 
 # acessando propriedades
 print(pessoa1.nome)
