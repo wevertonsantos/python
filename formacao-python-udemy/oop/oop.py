55e08d52bec8580c2c171cd3d641a710da6a2218
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-07 13:16:25 -0300

    criando classe e construtor

--- formacao-python-udemy/oop/oop.py
+++ formacao-python-udemy/oop/oop.py
@@ -3,3 +3,9 @@ class Teste: # criando classe(objeto) bem simples
 
 minha_classe = Teste() # instância do objeto
 print(type(minha_classe))
+
+class NossaClasse: # criando classe
+    def __init__(self): # criando construtor
+        print("Eu existo")
+
+var = NossaClasse()
