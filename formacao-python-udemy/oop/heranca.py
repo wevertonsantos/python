4624672e41c6883fd1fd443ce1ff4192a53c9db2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 13:10:39 -0300

    criando propriedade

--- formacao-python-udemy/oop/heranca.py
+++ formacao-python-udemy/oop/heranca.py
@@ -4,13 +4,14 @@ class FormaGeometrica:
         self.largura = largura
 
 class Quadrado(FormaGeometrica): # herdando outra classe
-    pass
+    lado = 10 # criando propriedade
 
 class Triangulo(FormaGeometrica):
-    pass
+    angulo = 30
 
 quadrado = Quadrado(100,50) # instânciando com as propriedades da classe pai
 triangulo = Triangulo(100,50)
 
 print(quadrado.altura)
 print(quadrado.largura)
+print(quadrado.lado)
