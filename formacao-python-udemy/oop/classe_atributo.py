23007050b59ae754411d9b66734e265f27128a67
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 13:00:51 -0300

    atribuindo uma instância da classe dentro da lista

--- formacao-python-udemy/oop/classe_atributo.py
+++ formacao-python-udemy/oop/classe_atributo.py
@@ -8,3 +8,14 @@ class Tipo2:
 classe2 = Tipo2()
 classe1 = Tipo1(classe2) # passando a instância da classe como parâmetro.
 print(classe1.outra.numero) # printando propriedade número atráves da classe
+
+class Exemplo:
+    def __init__(self):
+        pass
+
+lista = []
+ex1 = Exemplo
+ex2 = Exemplo
+lista.append(ex1) # atribuindo uma instância da classe dentro da lista
+lista.append(ex2)
+print(lista)
