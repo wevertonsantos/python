076446cb5db369b5bf9b77e0254f89f730e77a58
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 13:14:08 -0300

    tenho acesso ao método da classe pai.

--- formacao-python-udemy/oop/heranca.py
+++ formacao-python-udemy/oop/heranca.py
@@ -2,6 +2,8 @@ class FormaGeometrica:
     def __init__(self,altura,largura):
         self.altura = altura
         self.largura = largura
+    def funcao_herdada(self):
+        print(f"Sou herdado")
 
 class Quadrado(FormaGeometrica): # herdando outra classe
     lado = 10 # criando propriedade
@@ -15,3 +17,4 @@ triangulo = Triangulo(100,50)
 print(quadrado.altura)
 print(quadrado.largura)
 print(quadrado.lado) # é possível verificar as propriedades da classe filho
+quadrado.funcao_herdada() # tenho acesso ao método da classe pai.
