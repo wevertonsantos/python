af6fde65a423b1e37c055f7403c2e4ea50a1a3b7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 12:55:27 -0300

    passando a instância da classe como parâmetro.

--- formacao-python-udemy/oop/classe_atributo.py
+++ formacao-python-udemy/oop/classe_atributo.py
@@ -1,3 +1,9 @@
 class Tipo1:
     def __init__(self, outra_classe):
         self.outra = outra_classe # propriedade que é outra classe
+
+class Tipo2:
+    numero = 10
+
+classe2 = Tipo2()
+classe1 = Tipo1(classe2) # passando a instância
