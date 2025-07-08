664fe21ed75cfe7192bffa484a3a5c307ec69d5b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 12:44:01 -0300

    # criando propriedade dentro da classe

--- formacao-python-udemy/oop/funcoes_objeto.py
+++ formacao-python-udemy/oop/funcoes_objeto.py
@@ -1,11 +1,12 @@
 class Pessoa:
-    def __init__(self,nome,idade):
+    def __init__(self,nome):
         self.nome = nome
-        self.idade = idade
     def print_string(self,nome):
         print(f"Meu nome é: {nome}")
     def print_nome(self):
         self.print_string(self.nome) # chamando função do próprio objeto
+    def insere_idade(self,idade):
+        self.idade = idade # criando propriedade dentro da classe
 
-pessoa1 = Pessoa("Rodrigo","34")
+pessoa1 = Pessoa("Rodrigo")
 pessoa1.print_nome() # chamando função da instância do objeto
