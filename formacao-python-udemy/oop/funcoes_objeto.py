cb8997b46c3894736677b71fc2df22f1ad6fac48
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 12:36:18 -0300

    criando função dentro do objeto, chamando self como parâmetro.

--- formacao-python-udemy/oop/funcoes_objeto.py
+++ formacao-python-udemy/oop/funcoes_objeto.py
@@ -2,4 +2,5 @@ class Pessoa:
     def __init__(self,nome,idade):
         self.nome = nome
         self.idade = idade
-        
+    def print_nome(self):
+        print(f"Meu nome é: {self.nome}")
