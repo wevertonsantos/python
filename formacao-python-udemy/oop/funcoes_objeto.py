2dcfd7a3cd80b98aa95d07d44bb8c96597b0d47a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 12:41:32 -0300

    chamando função do próprio objeto

--- formacao-python-udemy/oop/funcoes_objeto.py
+++ formacao-python-udemy/oop/funcoes_objeto.py
@@ -2,8 +2,10 @@ class Pessoa:
     def __init__(self,nome,idade):
         self.nome = nome
         self.idade = idade
+    def print_string(self,nome):
+        print(f"Meu nome é: {nome}")
     def print_nome(self):
-        print(f"Meu nome é: {self.nome}")
+        self.print_string(self.nome) # chamando função do próprio objeto
 
 pessoa1 = Pessoa("Rodrigo","34")
 pessoa1.print_nome() # chamando função da instância do objeto
