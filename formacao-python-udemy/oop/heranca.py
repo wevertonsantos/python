944556bb68daf0f49e4c6afe8ded44ae750b3694
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 13:04:48 -0300

    herdando outra classe

---
+++ formacao-python-udemy/oop/heranca.py
@@ -0,0 +1,7 @@
+class FormaGeometrica:
+    def __init__(self,altura,largura):
+        self.altura = altura
+        self.largura = largura
+
+class Quadrado(FormaGeometrica): # herdando outra classe
+    pass
