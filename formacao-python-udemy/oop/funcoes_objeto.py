e371c2353e6f560b238e3a62fa2b8a6d572fabb0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-08 12:37:26 -0300

    chamando função da instância do objeto

--- formacao-python-udemy/oop/funcoes_objeto.py
+++ formacao-python-udemy/oop/funcoes_objeto.py
@@ -4,3 +4,6 @@ class Pessoa:
         self.idade = idade
     def print_nome(self):
         print(f"Meu nome é: {self.nome}")
+
+pessoa1 = Pessoa("Rodrigo","34")
+pessoa1.print_nome() # chamando função da instância do objeto
