1126b89703f485f10318c788bb9642fc92c2fa52
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-09 13:37:18 -0300

    sobrescrevendo aumenta marcha da classe pai

--- formacao-python-udemy/oop/override3.py
+++ formacao-python-udemy/oop/override3.py
@@ -7,3 +7,15 @@ class Veiculo:
     def diminui_marcha(self):
         self.marcha -= 1
         self.marcha = min(self.marcha,5)
+
+class Palio(Veiculo):
+    def __init__(self, marcha):
+        Veiculo.__init__(self,marcha)
+
+class EcoSport(Veiculo):
+    def __init__(self, marcha):
+        Veiculo.__init__(self,marcha)
+
+    def aumenta_marcha(self): # sobrescrevendo aumenta marcha da classe pai
+        self.marcha += 1
+        self.marcha = min(self.marcha,6)
