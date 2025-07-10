570c82bd74927dac49a48da43a880dca50096c3c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-10 12:27:35 -0300

    criando função privada

--- formacao-python-udemy/oop/modificadores_acesso.py
+++ formacao-python-udemy/oop/modificadores_acesso.py
@@ -2,5 +2,12 @@ class Segredo:
     def __init__(self):
         self.__segredo = 'Senha' # propriedade privada
 
+    def __printa_segredo(self): # função privada
+        print(self.__segredo)
+
+    def printa_segredo(self):
+        self.__printa_segredo()
+
+
 seg = Segredo()
 print(seg.__segredo)
