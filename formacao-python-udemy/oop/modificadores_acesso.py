c738bd02675c242a9185206f2bf833ed5bd5066a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-10 12:46:58 -0300

    criando property para pegar nome

--- formacao-python-udemy/oop/modificadores_acesso.py
+++ formacao-python-udemy/oop/modificadores_acesso.py
@@ -11,3 +11,13 @@ class Segredo:
 
 seg = Segredo()
 print(seg.__segredo)
+
+class Pessoa:
+    def __init__(self,nome):
+        self.__nome = nome
+    
+    def get_nome(self):
+        print("pegando nome")
+        return self.__nome
+
+    nome = property(get_nome) # criando property para pegar nome
