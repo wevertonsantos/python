25821acc282c1ba7987c3e2aa12b1d7ac9ee908a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 12:03:59 -0300

    trabalhando com class method e static method

---
+++ formacao-python-udemy/oop/metodos_estaticos.py
@@ -0,0 +1,11 @@
+class Teste:
+    def __init__(self,gasolina):
+        pass
+
+    @classmethod
+    def class_method(cls):
+        print(cls)
+
+    @staticmethod
+    def static_method():
+        print("static method")
