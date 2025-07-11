54f4e3c939a5f9ff5e9eaad7f0e85afc0d0d1172
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-11 11:46:10 -0300

    mais exemplo com property e setter

--- formacao-python-udemy/oop/protegendo_atributos.py
+++ formacao-python-udemy/oop/protegendo_atributos.py
@@ -16,3 +16,21 @@ class Natural:
 numero = Natural(10)
 numero.numero = 20 # invocando método set
 print(numero.numero) # invocando método get
+
+class Pessoa:
+    def __init__(self,nome):
+        self.__nome = nome
+    
+    @property
+    def nome(self):
+        return self.__nome.capitalize()
+    
+    @nome.setter
+    def nome(self,value):
+        if len(value) != 0:
+            self.__nome = value
+
+pessoa = Pessoa("rodrigo")
+print(pessoa.nome)
+pessoa.nome = 'fernando'
+print(pessoa.nome) 
