bbaa44377d5f993f397ab44425a9b88ad11ec903
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-12 13:46:15 -0300

    rotate array

---
+++ leetcode/top-interview-150/rotate_array.py
@@ -0,0 +1,23 @@
+'''
+https://leetcode.com/problems/rotate-array/description/?envType=study-plan-v2&envId=top-interview-150
+
+Given an integer array nums, rotate the array to the right by k steps, where k is non-negative.
+
+Example 1:
+
+Input: nums = [1,2,3,4,5,6,7], k = 3
+Output: [5,6,7,1,2,3,4]
+Explanation:
+rotate 1 steps to the right: [7,1,2,3,4,5,6]
+rotate 2 steps to the right: [6,7,1,2,3,4,5]
+rotate 3 steps to the right: [5,6,7,1,2,3,4]
+'''
+
+nums = [1,2,3,4,5,6,7]
+k = 3
+i = 0
+if k > 0:
+    while i < k:
+        print(nums[nums.index(k):((-k -1))])
+        print(nums[((k -1)):])
+        i += 1
