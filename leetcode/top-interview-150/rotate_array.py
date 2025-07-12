b63ba081564ee4841f661f0e6f59540f1ee824c1
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-12 14:06:04 -0300

    terminando problema;

--- leetcode/top-interview-150/rotate_array.py
+++ leetcode/top-interview-150/rotate_array.py
@@ -13,11 +13,11 @@ rotate 2 steps to the right: [6,7,1,2,3,4,5]
 rotate 3 steps to the right: [5,6,7,1,2,3,4]
 '''
 
-nums = [1,2,3,4,5,6,7]
-k = 3
+nums = [-1,-100,3,99]
+k = 2
 i = 0
 if k > 0:
     while i < k:
-        print(nums[nums.index(k):((-k -1))])
-        print(nums[((k -1)):])
+        nums.insert(0,nums.pop())
         i += 1
+print(nums)
