d512bc7f1ab5d511f52d3943270cf31bf82d2576
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-13 19:34:26 -0300

    evento especificando quando a classe for deletada

--- formacao-python-udemy/oop/exclusao_objetos.py
+++ formacao-python-udemy/oop/exclusao_objetos.py
@@ -4,7 +4,9 @@ del numero # deletando variável da memória
 
 class Teste:
     def __init__(self):
-        pass
+        self.lista = [1,2,3]
+    def __del__(self): # evento especificando quando a classe for deletada
+        print("Fui deletado")
 
 variavel = Teste()
 del variavel # deletando instância do objeto
