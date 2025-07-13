3dd58b8e6f8acc1683b46e0a8dfa4ebacb769c3a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-13 12:32:06 -0300

    corrigindo erros.

--- codewars/8kyu/correct_mistakes_character_recognition.py
+++ codewars/8kyu/correct_mistakes_character_recognition.py
@@ -4,7 +4,7 @@ https://www.codewars.com/kata/577bd026df78c19bca0002c0/train/python
 8 kyu
 Correct the mistakes of the character recognition software
 '''
-
+s = "51NGAP0RE"
 def correct(s):
     return s.replace("5","S").replace("0","O").replace("1","I")
 print(correct(s))
