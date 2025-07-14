ce13b889b4ab1e48978e8a903a6807ceeb616ade
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-14 11:25:16 -0300

    verificação se classe herdada é do tipo base

--- formacao-python-udemy/oop/tipos_objeto.py
+++ formacao-python-udemy/oop/tipos_objeto.py
@@ -2,6 +2,16 @@ class Base:
     def __init__(self):
         pass
 
+class Herdeiro(Base):
+    def __init__(self):
+        pass
+
 classe = Base()
 e_base = isinstance(classe, Base) # verificando se a instancia do objeto é do tipo base classe
 print(e_base)
+
+classe = Herdeiro()
+e_base = isinstance(classe,Base) 
+e_herdero = isinstance(classe,Herdeiro) # verificação se classe herdada é do tipo base
+print(e_base)
+print(e_herdero)
