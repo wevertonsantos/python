47218634c3eac07f4f697aae7c219265ffe9a186
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-16 10:58:46 -0300

    verificando argumentos passado no prompt de comando junto com o nome do arquivo

--- formacao-python-udemy/modulos/main.py
+++ formacao-python-udemy/modulos/main.py
@@ -1,5 +1,8 @@
+import sys
+
 def main():
     print("Início do programa")
+    print(sys.argv) # verificando argumentos passado no prompt de comando junto com o nome do arquivo
 
 if __name__ == '__main__': # verificando se é o nome é o principal
     main()
