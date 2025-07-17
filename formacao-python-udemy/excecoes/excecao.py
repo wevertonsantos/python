b8289e5d436ab71a636d00f329dc97e445eddd10
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-17 12:54:13 -0300

    finally irá executar de qualquer forma

--- formacao-python-udemy/excecoes/excecao.py
+++ formacao-python-udemy/excecoes/excecao.py
@@ -3,3 +3,10 @@ try: # tentando fazer a ação
     print(lista[10])
 except Exception as error: # irá ser executado caso apresente o erro
     print("Falha ao acessar. Mensagem:", str(error)) # mostrando mensagem original da exceção com Exception
+
+try:
+    print(lista[10])
+except:
+    print("Falha ao acessar")
+finally: # finally irá executar de qualquer forma
+    print("Executa sempre que o try-except acabar")
