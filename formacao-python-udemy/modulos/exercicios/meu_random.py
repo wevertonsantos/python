06e2368cca2d7e8c1eaf7b721dab20daefb825ff
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-17 11:48:59 -0300

    terminando módulo;

--- formacao-python-udemy/modulos/exercicios/meu_random.py
+++ formacao-python-udemy/modulos/exercicios/meu_random.py
@@ -1,8 +1,8 @@
 from random import randrange
 def get_random_lista(inicial, final, tam):
     lista = []
-    for i in (0,tam):
+    i = 0
+    while i < tam:
         lista.append(randrange(inicial,final))
+        i += 1
     return lista
-
-print(get_random_lista(100,200,4))
