f4d6c2cb58e3d844b4cfbed09c769227ddda1cf4
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-17 12:39:43 -0300

    # mostrando mensagem original da exceção com Exception

--- formacao-python-udemy/excecoes/excecao.py
+++ formacao-python-udemy/excecoes/excecao.py
@@ -1,5 +1,5 @@
 lista = [1,2,3]
 try: # tentando fazer a ação
     print(lista[10])
-except: # irá ser executado caso apresente o erro
-    print("Falha ao acessar")
+except Exception as error: # irá ser executado caso apresente o erro
+    print("Falha ao acessar. Mensagem:", str(error)) # mostrando mensagem original da exceção com Exception
