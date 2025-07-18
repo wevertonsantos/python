d7244d45386dbecf86ca9c2980ae9dc8aa9dff07
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-18 09:13:11 -0300

    Ex. 2

--- formacao-python-udemy/excecoes/exercicios/atividades.py
+++ formacao-python-udemy/excecoes/exercicios/atividades.py
@@ -11,3 +11,17 @@ def soma(s1,s2):
         print("Valores que você digitou não é válido")
 
 print(soma('10','s'))
+
+'''
+2 - Crie uma função que receba uma lista e um número e retorne o elemento
+da lista na posição deste número. Faça um tratamento para que caso haja um
+acesso fora do índice a função retorne o valor None.
+'''
+
+def funcao(lista,num):
+    try:
+        return lista[num]
+    except:
+        return None
+
+print(funcao([1,2,3,4,5,6],10))
