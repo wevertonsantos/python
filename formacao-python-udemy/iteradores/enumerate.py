c4449b8438e52f668bdbc4ba95c7470421bd7c8b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-19 10:37:25 -0300

    colocando indice e valor em variáveis

--- formacao-python-udemy/iteradores/enumerate.py
+++ formacao-python-udemy/iteradores/enumerate.py
@@ -1,4 +1,4 @@
 # transforma um objeto interável em uma tupla onde vai ter indice e o objeto
 lista = [1,2,3,4,5]
-for item in enumerate(lista):
-    print(item)
+for indice,valor in enumerate(lista): # colocando indice e valor em variáveis
+    print(indice, valor)
