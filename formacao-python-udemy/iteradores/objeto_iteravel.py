9eaeb372a27fc497d8c5914fa141136ba68dd1d3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-19 08:28:44 -0300

    criando objeto iteravel que vai retornar o próximo número.

---
+++ formacao-python-udemy/iteradores/objeto_iteravel.py
@@ -0,0 +1,15 @@
+class ColecaoNumeros:
+    def __init__(self,numero_maximo):
+        self.max = numero_maximo
+    def __iter__(self):
+        self.numero_atual = 0
+        return self
+    
+    def __next__(self): # vai retornar o próximo número
+        if self.numero_atual <= self.max:
+            retorno = self.numero_atual
+            self.numero_atual += 1
+            return retorno
+        else:
+            raise StopIteration
+        
