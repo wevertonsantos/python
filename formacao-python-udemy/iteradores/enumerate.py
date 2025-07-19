ff85a5f30256e8e6dab887065fb9703b6d5d845d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-19 10:41:29 -0300

    objeto iterável

--- formacao-python-udemy/iteradores/enumerate.py
+++ formacao-python-udemy/iteradores/enumerate.py
@@ -1,4 +1,4 @@
-# transforma um objeto interável em uma tupla onde vai ter indice e o objeto
+# transforma um objeto iterável em uma tupla onde vai ter indice e o objeto
 lista = [1,2,3,4,5]
 for indice,valor in enumerate(lista): # colocando indice e valor em variáveis
     print(indice, valor)
