07d0c391ee30bb2dfed2e9fc18734fb3263643a1
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-21 10:54:47 -0300

    utilizando join.

---
+++ formacao-python-udemy/iteradores/join.py
@@ -0,0 +1,8 @@
+# fazendo join
+
+texto1 = "olá"
+print("#".join(texto1)) # a cada letra do texto1 irá colocar #
+
+lista = ['a','b','c','d']
+letras = ' '.join(lista) # a cada letra da lista irá ter espaço
+print(letras)
