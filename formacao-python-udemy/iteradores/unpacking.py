d259f2e591f06ca5cb5460cfb590928680fabcce
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-21 10:47:50 -0300

    unpacking com yield

--- formacao-python-udemy/iteradores/unpacking.py
+++ formacao-python-udemy/iteradores/unpacking.py
@@ -4,3 +4,11 @@ produtos = [
 
 for produto,valor in produtos: # atribuindo produto e valor em duas variáveis
     print(produto,valor)
+
+def gen1():
+    yield [1,2]
+    yield [3,4]
+    yield [5,6]
+
+for x,y in gen1(): #unpacking com yield
+    print(x,y)
