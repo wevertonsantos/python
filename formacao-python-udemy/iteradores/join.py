9c488670496472fa650ee856dd52ad387e56dc70
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-21 10:57:15 -0300

    mais exemplos com join.

--- formacao-python-udemy/iteradores/join.py
+++ formacao-python-udemy/iteradores/join.py
@@ -6,3 +6,6 @@ print("#".join(texto1)) # a cada letra do texto1 irá colocar #
 lista = ['a','b','c','d']
 letras = ' '.join(lista) # a cada letra da lista irá ter espaço
 print(letras)
+
+letras = '-'.join([str(i) for i in range(10)])
+print(letras)
