71bea9b80cf8a63d082363ba3d13b256624e07c7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-26 13:38:32 -0300

    Ex. 6

--- formacao-python-udemy/iteradores/exercicios/atividades.py
+++ formacao-python-udemy/iteradores/exercicios/atividades.py
@@ -85,3 +85,13 @@ def meses_enum():
 
 for indice, mes in meses_enum():
   print(indice+1, mes)
+
+# 6 - Crie uma função que receba uma lista de frases e junte as mesmas em uma só, 
+# separados por ponto final.
+
+def frase(lista):
+  return '. '.join(lista) + '.'
+
+textos = ['Olá, sou Carlos','Gosto de Python', 'Trabalho como dev']
+
+print(frase(textos))
