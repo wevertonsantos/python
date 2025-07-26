bb8273959186f01b43aef23989f2180663352b50
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-26 13:37:47 -0300

    Ex. 5

--- formacao-python-udemy/iteradores/exercicios/atividades.py
+++ formacao-python-udemy/iteradores/exercicios/atividades.py
@@ -72,3 +72,16 @@ class Fatorial:
 
 for i in Fatorial(1,10):
   print(i)
+
+# 5 - Utilizando como base o exercício 1, retorne o número que representa o mês 
+# e o próprio mês. Faça isso de duas maneiras diferentes 
+# (usando enumeradores e a outra usando join).
+
+def meses_enum():
+  meses = ['janeiro','fevereiro','março','abril','maio','junho','julho',
+           'agosto','setembro','outubro','novembro','dezembro']
+  for i in enumerate(meses):
+    yield i
+
+for indice, mes in meses_enum():
+  print(indice+1, mes)
