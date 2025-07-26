d5e85cf17191064ac8b7d9a0a6a0e9fa391b8b30
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-26 13:37:22 -0300

    Ex. 4

--- formacao-python-udemy/iteradores/exercicios/atividades.py
+++ formacao-python-udemy/iteradores/exercicios/atividades.py
@@ -43,3 +43,32 @@ tabuada_cal = Tabuada(2)
 
 for i in tabuada_cal:
   print(i)
+
+# 4 (Desafio) - Crie uma classe que retorne os fatoriais de um número no 
+#  intervalo de X a Y, recebidos por parâmetro no construtor da classe.
+
+class Fatorial:
+  def __init__(self,x,y):
+    self.x = x
+    self.y = y
+  
+  def __iter__(self):
+    self.atual = self.x
+    return self
+  
+  @staticmethod
+  def calcula_fatorial(num):
+    result = 1
+    for i in range(1, num+1):
+      result *= i
+    return result
+  
+  def __next__(self):
+    if (self.atual == self.y + 1):
+      raise StopIteration
+    result = Fatorial.calcula_fatorial(self.atual)
+    self.atual += 1
+    return result
+
+for i in Fatorial(1,10):
+  print(i)
