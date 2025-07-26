7b1647e962f6ad74235b56b3a775cdc57f758e34
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-26 13:34:47 -0300

    Ex. 2

--- formacao-python-udemy/iteradores/exercicios/atividades.py
+++ formacao-python-udemy/iteradores/exercicios/atividades.py
@@ -9,3 +9,15 @@ def meses():
 
 for mes in meses():
   print(mes)
+
+# 2 - Cria uma função iterável que receba uma lista de números e que retorne 
+# a cada iteração um item dessa lista multiplicado por dois.
+
+def duplicado(lista):
+  for i in lista:
+    yield i*2
+
+lista = [1,2,3,4,5]
+
+for i in duplicado(lista):
+  print(i)
