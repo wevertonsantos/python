c7ff60f640108e5d01abdd4c17d52e4b18815170
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-26 13:36:21 -0300

    Ex. 3

--- formacao-python-udemy/iteradores/exercicios/atividades.py
+++ formacao-python-udemy/iteradores/exercicios/atividades.py
@@ -21,3 +21,25 @@ lista = [1,2,3,4,5]
 
 for i in duplicado(lista):
   print(i)
+
+# 3 - Crie uma classe iterável chamada “Tabuada” que calcule a tabuada da 
+# multiplicação do número recebido no construtor. 
+# A cada iteração ela deve retornar um resultado da tabuada. 
+# Para testar use um laço for.
+
+class Tabuada:
+  def __init__(self, num):
+    self.num = num
+  def __iter__(self):
+    self.atual = 0
+    return self
+  def __next__(self):
+    self.atual += 1
+    if(self.atual ==11):
+      raise StopIteration
+    return self.atual * self.num
+
+tabuada_cal = Tabuada(2)
+
+for i in tabuada_cal:
+  print(i)
