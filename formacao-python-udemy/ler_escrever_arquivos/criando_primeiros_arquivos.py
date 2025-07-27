694e0bf1384988a4970f6c079ea01aae8d703420
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-27 17:48:21 -0300

    escrevendo uma linha no arquivo e fechando arquivo.

--- formacao-python-udemy/ler_escrever_arquivos/criando_primeiros_arquivos.py
+++ formacao-python-udemy/ler_escrever_arquivos/criando_primeiros_arquivos.py
@@ -1,1 +1,4 @@
 arquivo = open("exemplo.txt","wt") # "w" abre um arquivo para escrita, se não existir o arquivo é criado/ "t" abre um arquivo em modo texto
+arquivo.write("Olá estou escrevendo no arquivo\n") # escrevendo uma linha no arquivo
+arquivo.write("Está é a segunda linha do arquivo")
+arquivo.close() # fecha o arquivo
