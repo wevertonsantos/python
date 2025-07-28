72cee9bff555fde8c413ed23fa5b7dc84aefabde
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-28 09:59:52 -0300

    lendo todo o arquivo com read(). r - abre um arquivo para leitura, se não existir ele gera exceção.

--- formacao-python-udemy/ler_escrever_arquivos/lendo_arquivos.py
+++ formacao-python-udemy/ler_escrever_arquivos/lendo_arquivos.py
@@ -1,2 +1,6 @@
 with open("exemplo_with.txt",'wt') as arquivo: # nessa abordagem com with não é preciso fechar porque ele vai criar e fechar com with
     arquivo.write("Olá")
+
+# abrir arquivo que foi gerado
+arquivo = open("exemplo.txt","rt") # r - abre um arquivo para leitura, se não existir ele gera exceção.
+lido = arquivo.read() # lendo todo o arquivo com read()
