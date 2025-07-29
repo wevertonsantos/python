bba106f8804c145ca1104631a8e7749ebaaa0067
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-29 11:12:19 -0300

    criando pastas e dentro das pastar um arquivo texto.

--- formacao-python-udemy/ler_escrever_arquivos/criando_excluindo_pastas.py
+++ formacao-python-udemy/ler_escrever_arquivos/criando_excluindo_pastas.py
@@ -1,3 +1,15 @@
 import os
 os.mkdir('pasta') # criando pasta através do os
 os.rmdir('pasta') # excluindo pasta (deve estar vazia)
+
+for i in range(0,2):
+    nome_pasta = f"pasta_{i}"
+    try:
+        os.mkdir(nome_pasta)
+    except:
+        pass
+
+    try:
+        open(nome_pasta + '/texto.txt','wt').close()
+    except:
+        pass
