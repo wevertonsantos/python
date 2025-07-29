fb2156400d53b3cd4c0b2a4f3b12a841900e99e7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-29 10:39:43 -0300

    fechando mesmo se houver erro

--- formacao-python-udemy/ler_escrever_arquivos/verificar_tratar_erros.py
+++ formacao-python-udemy/ler_escrever_arquivos/verificar_tratar_erros.py
@@ -5,3 +5,9 @@ if arquivo_existe:
     print("O arquivo existe")
 else:
     print("O arquivo não existe")
+
+file = open("teste.txt",'wt')
+try:
+    file.write('hello, world')
+finally:
+    file.close() # fechando mesmo se houver erro
