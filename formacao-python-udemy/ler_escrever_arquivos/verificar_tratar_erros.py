1cb5827c4cc0c3002b3ef0761162745ed8526f5c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-29 10:31:55 -0300

    fazendo verificação de arquivo.

--- formacao-python-udemy/ler_escrever_arquivos/verificar_tratar_erros.py
+++ formacao-python-udemy/ler_escrever_arquivos/verificar_tratar_erros.py
@@ -1,2 +1,7 @@
 from os import path
 arquivo_existe = path.exists("exemplo.txt") # verificando se arquivo existe
+
+if arquivo_existe:
+    print("O arquivo existe")
+else:
+    print("O arquivo não existe")
