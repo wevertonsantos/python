0eea7597a4d8aab8559cdadf8ac1b003a3b0d223
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-30 10:17:42 -0300

    permite criar arquivo csv e escrever nele (passando o arquivo e um delimitador)

--- formacao-python-udemy/ler_escrever_arquivos/arquivos_csv.py
+++ formacao-python-udemy/ler_escrever_arquivos/arquivos_csv.py
@@ -1,3 +1,5 @@
 #criar arquivo csv com a biblioteca
 import csv
 
+with open("pessoas.csv","w") as arquivo:
+    escritorCsv = csv.writer(arquivo,delimiter=',') # permite criar arquivo csv e escrever nele (passando o arquivo e um delimitador)
