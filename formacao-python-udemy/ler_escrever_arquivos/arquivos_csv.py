81cf8db974bc4153e2a827b62fe65be512b6500a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-30 10:37:36 -0300

    criar arquivo csv apartir de um conteúdo de uma lista

--- formacao-python-udemy/ler_escrever_arquivos/arquivos_csv.py
+++ formacao-python-udemy/ler_escrever_arquivos/arquivos_csv.py
@@ -5,3 +5,10 @@ with open("pessoas.csv","w") as arquivo:
     escritorCsv = csv.writer(arquivo,delimiter=',') # permite criar arquivo csv e escrever nele (passando o arquivo e um delimitador)
     escritorCsv.writerow(["id","nome"]) #escrever uma linha no arquivo csv (passando nome das colunas)
     escritorCsv.writerow(["1","Fernando"])
+
+# criar arquivo csv apartir de um conteúdo de uma lista
+linhas = [["id","nome"],["1","Fernando"]]
+
+with open('pessoas2.csv','w') as arquivo2:
+    escritorCsv = csv.writer(arquivo2)
+    escritorCsv.writerows(linhas) # escrevendo várias linhas ao mesmo tempo.
