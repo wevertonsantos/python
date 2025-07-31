087e2644d064c8cbf4657356321ac84c7a92d217
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-07-31 10:21:22 -0300

    lendo arquivo json e transformando em dicionário

--- formacao-python-udemy/ler_escrever_arquivos/tratando_json.py
+++ formacao-python-udemy/ler_escrever_arquivos/tratando_json.py
@@ -22,3 +22,11 @@ texto = json.dumps(DadosPessoas, indent=4) #identar objetos com 4 espaços
 print(texto)
 with open('exemplo.json','wt') as arquivo: # criando arquivo como json
     arquivo.write(texto) #escrevendo o dump no arquivo
+
+#lendo arquivo json e transformando em dicionário
+dicionario = None
+with open('exemplo.json','rt') as arquivo:
+    arquivo_lido = arquivo.read()
+    dicionario = json.loads(arquivo_lido) # transformando arquivo lido para dicionário com load
+
+print(dicionario)
