04976e618f5bcb7f3dd0261cf3a4c7adda0cd0ff
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-01 09:07:12 -0300

    criando sub elemento de um elemento

--- formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
@@ -3,3 +3,5 @@ import os
 
 no_raiz = xml.Element("DadosPessoais") # criando um nó raiz com um elemento
 no_pessoa = xml.Element("Pessoa",attrib={'nome':'Rodrigo'}) # criando elemento com atributo
+no_cpf = xml.SubElement(no_pessoa,'CPF') # criando sub elemento de um elemento
+no_cpf.text = '123456789'
