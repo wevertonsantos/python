b21003525ab7b873aeacfb4639f32cd16c8810be
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-01 09:22:55 -0300

    criando função para melhorar legibilidade

--- formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
@@ -1,10 +1,15 @@
 import xml.etree.ElementTree as xml #importando biblioteca xml.
 import os
 
+def cria_tag_pessoa(nome,cpf):
+    no_pessoa = xml.Element("Pessoa",attrib={'Nome':nome}) # criando elemento com atributo
+
+    no_cpf = xml.SubElement(no_pessoa,'CPF') # criando sub elemento de um elemento
+    no_cpf.text = cpf
+
+    return no_pessoa
+
 no_raiz = xml.Element("DadosPessoais") # criando um nó raiz com um elemento
-no_pessoa = xml.Element("Pessoa",attrib={'Nome':'Rodrigo'}) # criando elemento com atributo
-no_cpf = xml.SubElement(no_pessoa,'CPF') # criando sub elemento de um elemento
-no_cpf.text = '123456789'
 
 no_raiz.append(no_pessoa) # passando nó pessoa para o nó raiz
 
