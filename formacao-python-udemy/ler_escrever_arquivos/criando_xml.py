309a7359a2b13f347ee46d5921a39c7837137947
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-01 09:11:08 -0300

    passando nó pessoa para o nó raiz

--- formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
@@ -2,6 +2,8 @@ import xml.etree.ElementTree as xml #importando biblioteca xml.
 import os
 
 no_raiz = xml.Element("DadosPessoais") # criando um nó raiz com um elemento
-no_pessoa = xml.Element("Pessoa",attrib={'nome':'Rodrigo'}) # criando elemento com atributo
+no_pessoa = xml.Element("Pessoa",attrib={'Nome':'Rodrigo'}) # criando elemento com atributo
 no_cpf = xml.SubElement(no_pessoa,'CPF') # criando sub elemento de um elemento
 no_cpf.text = '123456789'
+
+no_raiz.append(no_pessoa) # passando nó pessoa para o nó raiz
