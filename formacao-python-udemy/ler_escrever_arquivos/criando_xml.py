9e33dd02ca86e7f9e07b8a8a40c781b58daf8fdd
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-01 09:03:20 -0300

    criando elemento com atributo

--- formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/criando_xml.py
@@ -2,3 +2,4 @@ import xml.etree.ElementTree as xml #importando biblioteca xml.
 import os
 
 no_raiz = xml.Element("DadosPessoais") # criando um nó raiz com um elemento
+no_pessoa = xml.Element("Pessoa",attrib={'nome':'Rodrigo'}) # criando elemento com atributo
