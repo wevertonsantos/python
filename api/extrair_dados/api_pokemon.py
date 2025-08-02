0d91361eaf199577e7d77051aa7325aef5781671
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 13:56:02 -0300

    retornando o conteúdo com .text do response

--- api/extrair_dados/api_pokemon.py
+++ api/extrair_dados/api_pokemon.py
@@ -5,6 +5,6 @@ url = "https://pokeapi.co/api/v2/pokemon/" # passando url
 payload = {}
 headers = {}
 
-response = requests.request("GET", url, headers=headers, data=payload) # retornando a requisição com método get e passando a url
+response = requests.request("GET", url, headers=headers, data=payload).text # retornando o conteúdo com .text do response
 
 print(response.text)
