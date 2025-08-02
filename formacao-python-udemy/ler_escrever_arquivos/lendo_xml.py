d494b116db34a7c180677ef0790e26435822174d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 11:15:58 -0300

    fazendo loop dentro da raiz e impirimindo pessoa e atributo que é o nome e depois percorrendo a tag pessoa com os sub elementos

--- formacao-python-udemy/ler_escrever_arquivos/lendo_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/lendo_xml.py
@@ -2,3 +2,7 @@ import xml.etree.ElementTree as xml
 tree = xml.parse("dados_pessoais3.xml") # construindo estrutura de árvore com arquivo xml
 root = tree.getroot() #metodo que acessa a raiz do arquivo
 print(root.tag) # impressão pela raiz
+for pessoa in root:
+    print(' ',pessoa.tag,pessoa.attrib['Nome']) # imprimindo pessoa raiz, o nome da pessoa
+    for dado in pessoa: # percorrendo tag pessoa
+        print(' ', dado.tag,dado.text)
