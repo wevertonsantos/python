7948ea71e3bd2952555af5a082a47f6b6f70bcc4
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 11:45:36 -0300

    pegando database do yaml

--- formacao-python-udemy/ler_escrever_arquivos/lendo_formato_yaml.py
+++ formacao-python-udemy/ler_escrever_arquivos/lendo_formato_yaml.py
@@ -1,3 +1,4 @@
 import yaml
 with open("config.yaml") as file:
     data = yaml.load(file, Loader=yaml.FullLoader) # carregando todo arquivo de configuração yaml
+    print(data['database']['name']) # pegando database do yaml
