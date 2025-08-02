c7a128c26fbced508b6a47e686308c349f8af91c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 11:07:20 -0300

    impressão pela raiz

--- formacao-python-udemy/ler_escrever_arquivos/lendo_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/lendo_xml.py
@@ -1,3 +1,4 @@
 import xml.etree.ElementTree as xml
 tree = xml.parse("dados_pessoais3.xml") # construindo estrutura de árvore com arquivo xml
 root = tree.getroot() #metodo que acessa a raiz do arquivo
+print(root.tag) # impressão pela raiz
