926193c39527593140c80980c51f30cbe83cf653
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 11:06:04 -0300

    metodo que acessa a raiz do arquivo

--- formacao-python-udemy/ler_escrever_arquivos/lendo_xml.py
+++ formacao-python-udemy/ler_escrever_arquivos/lendo_xml.py
@@ -1,2 +1,3 @@
 import xml.etree.ElementTree as xml
 tree = xml.parse("dados_pessoais3.xml") # construindo estrutura de árvore com arquivo xml
+root = tree.getroot() #metodo que acessa a raiz do arquivo
