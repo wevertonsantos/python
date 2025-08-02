1b196016007d4bc9c69a696ce735540ec8e4124a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 13:44:51 -0300

    retornando a requisição com método get e passando a url

--- api/extrair_dados/api_pokemon.py
+++ api/extrair_dados/api_pokemon.py
@@ -1,3 +1,10 @@
 import requests # importando requests para fazer requisição
 
-url = "https://pokeapi.co/api/v2/pokemon/bulbasaur" # passando url
+url = "https://pokeapi.co/api/v2/pokemon/" # passando url
+
+payload = {}
+headers = {}
+
+response = requests.request("GET", url, headers=headers, data=payload) # retornando a requisição com método get e passando a url
+
+print(response.text)
