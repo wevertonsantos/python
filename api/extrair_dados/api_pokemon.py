6e324a141c1aa27b2fd018051057a67704d8e389
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-02 14:14:43 -0300

    url da próxima página

--- api/extrair_dados/api_pokemon.py
+++ api/extrair_dados/api_pokemon.py
@@ -2,10 +2,13 @@ import requests,json # importando requests para fazer requisição
 
 url = "https://pokeapi.co/api/v2/pokemon/" # passando url
 
-payload = {}
-headers = {}
+while url != None:
+    payload = {}
+    headers = {}
 
-response = json.loads(requests.request("GET", url, headers=headers, data=payload).text) # transformando retorno o request em json
+    response = json.loads(requests.request("GET", url, headers=headers, data=payload).text) # transformando retorno o request em json
 
-for item_pokemon in response['results']: # acessando cada registro que retorna lista do results
-    print(item_pokemon)
+    url = response['next'] # url da próxima página
+
+    for item_pokemon in response['results']: # acessando cada registro que retorna lista do results
+        print(item_pokemon['name'])
