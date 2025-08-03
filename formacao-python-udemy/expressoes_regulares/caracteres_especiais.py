a80b77272e18282220c48aa90f3d54dc5de5de20
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 12:52:34 -0300

    \D irá buscar a não ocorrência de números

--- formacao-python-udemy/expressoes_regulares/caracteres_especiais.py
+++ formacao-python-udemy/expressoes_regulares/caracteres_especiais.py
@@ -7,3 +7,10 @@ info = re.search("\d",texto) # \d irá buscar qualquer número
 if info != None:
     print("Encontrado ocorrência em:", info.span())
     print("O que foi encontrado:", info.group())
+
+
+info = re.search("\D",texto) # \D irá buscar a não ocorrência de números
+
+if info != None:
+    print("Encontrado ocorrência em:", info.span())
+    print("O que foi encontrado:", info.group())
