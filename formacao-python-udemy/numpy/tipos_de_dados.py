8454331ecb53bb3346b154437481aa5ae88c26f5
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 14:36:57 -0300

    criando variável do tipo numpy string

--- formacao-python-udemy/numpy/tipos_de_dados.py
+++ formacao-python-udemy/numpy/tipos_de_dados.py
@@ -5,3 +5,6 @@ print(boolean,type(boolean))
 
 string = numpy.bytes_("este e um texto") # criando variável do tipo numpy bytes
 print(string)
+
+string = numpy.str_("este é um texto") # criando variável do tipo numpy string
+print(string)
