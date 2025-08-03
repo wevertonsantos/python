b925d5dc5d73aed16fdd3cfc98fd40bc1510144e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 14:01:14 -0300

    Ex. 6

--- formacao-python-udemy/expressoes_regulares/exercicios/atividades.py
+++ formacao-python-udemy/expressoes_regulares/exercicios/atividades.py
@@ -60,3 +60,16 @@ função “find all” para retornar as ocorrências.
 texto = "Olá, eu estou dormindo, e não sorrindo"
 info = re.findall("([\w]+indo|ando|endo)+", texto)
 print(info)
+
+'''
+6 - Faça um expressão regular para detectar se a hora é válida: O formato é de 24 horas, e é especificado da seguinte forma: HH:MM
+'''
+
+texto = '00:59'
+
+info = re.search("^([0-1][0-9]|[2][0-3]):[0-5][0-9]$", texto)
+
+if info != None:
+  print("Horário válido")
+else:
+  print("Horário inválido")
