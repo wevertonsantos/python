4fbc5718e654bf4895bd8ac79965ebecccd334a8
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 14:01:43 -0300

    Ex. 7

--- formacao-python-udemy/expressoes_regulares/exercicios/atividades.py
+++ formacao-python-udemy/expressoes_regulares/exercicios/atividades.py
@@ -73,3 +73,16 @@ if info != None:
   print("Horário válido")
 else:
   print("Horário inválido")
+
+'''
+7 (DESAFIO) - Faça uma expressão regular para validar se uma expressão é uma conta
+matemática valida. Nessa conta matemática só podem haver 2 números inteiros sendo
+somados ou subtraídos entre si. Valide se é uma expressão matemática ou não.
+'''
+
+texto = '3232+4453'
+info = re.search('^ *(\d+ *(-|\+) *\d+) *$', texto)
+if info != None:
+  print("Expressão válida")
+else:
+  print("Expressão inválida")
