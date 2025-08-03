b92aa9fc49c1ccf54eec14ba4493636066b67465
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 12:31:44 -0300

    '{}' - essa expressão irá buscar até N ocorrências, dependendo do que você passar

--- formacao-python-udemy/expressoes_regulares/grupos_de_padroes.py
+++ formacao-python-udemy/expressoes_regulares/grupos_de_padroes.py
@@ -8,3 +8,12 @@ if info != None:
     print('O que foi encontrado:',info.group())
 else:
     print("Nada foi encontrado")
+
+texto = 'abbabbabb'
+info = re.search('(abb){2}',texto) # '{}' - essa expressão irá buscar até N ocorrências, dependendo do que você passar
+
+if info != None:
+    print('Encontrado ocorrência em:',info.span())
+    print('O que foi encontrado:',info.group())
+else:
+    print("Nada foi encontrado")
