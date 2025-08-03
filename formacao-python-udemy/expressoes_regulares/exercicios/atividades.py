b177d46f03e8a55a84282bac86f1ae222318f8d0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 13:59:23 -0300

    Ex. 4

--- formacao-python-udemy/expressoes_regulares/exercicios/atividades.py
+++ formacao-python-udemy/expressoes_regulares/exercicios/atividades.py
@@ -31,3 +31,23 @@ possibilidades são:
 
 texto = 'Terça-Feira'
 info = re.search('^(Segunda-Feira|Terça-Feira|Quarta-Feira|Quinta-Feira|Sexta-Feira|Sábado|Domingo)$', texto)
+
+'''
+4 - Faça uma expressão regular para detectar telefones que comecem com 95. Telefones que começam com 95 devem ser bloqueados. Um número de telefone deve ser válido para poder ser validado, na forma
+XXXXXXXX onde X é um número. Primeiro diga se é um número válido. Caso seja diga se ele foi bloqueado
+ou não.
+'''
+
+texto = '9645634'
+
+info = re.search("^([0-9]{8})$", texto)
+
+if info !=None:
+  print("Número válido")
+  info2 = re.search("^95([0-9]{6})$", texto)
+  if info2 !=None:
+    print("Telefone bloqueado")
+  else:
+    print("Telefone não bloqueado")
+else:
+  print("Número inválido")
