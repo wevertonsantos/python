4a7831e1afe2d8e3fe4fb9604df2429ddfa8a0a3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 11:23:57 -0300

    span() significa que foi encontrado ocorrência em indices e group o que foi encontrado.

--- formacao-python-udemy/expressoes_regulares/expressoes_basico.py
+++ formacao-python-udemy/expressoes_regulares/expressoes_basico.py
@@ -3,3 +3,9 @@ import re # importando regular expressions
 # método search (buscar ocorrência de um determinado caracter em um texto)
 texto = '00123451'
 info = re.search('1',texto) # procurar '1' em texto
+
+if info != None:
+    print('Encontrado ocorrência em:',info.span())
+    print('O que foi encontrado:',info.group())
+else:
+    print("Nada foi encontrado")
