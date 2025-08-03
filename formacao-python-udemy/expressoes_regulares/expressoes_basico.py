11c1844eb2349648f2fa7313bc53f5508ad3c87e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 11:21:15 -0300

    método search (buscar ocorrência de um determinado caracter em um texto)

--- formacao-python-udemy/expressoes_regulares/expressoes_basico.py
+++ formacao-python-udemy/expressoes_regulares/expressoes_basico.py
@@ -1,1 +1,5 @@
 import re # importando regular expressions
+
+# método search (buscar ocorrência de um determinado caracter em um texto)
+texto = '00123451'
+info = re.search('1',texto) # procurar '1' em texto
