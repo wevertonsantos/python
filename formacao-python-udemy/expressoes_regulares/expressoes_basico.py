a3a0a4f0cfac4284038ab07af4a283f8c7b8c9b3
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 11:56:40 -0300

    mudando variáveis.

--- formacao-python-udemy/expressoes_regulares/expressoes_basico.py
+++ formacao-python-udemy/expressoes_regulares/expressoes_basico.py
@@ -24,13 +24,13 @@ info = re.sub("1","#",texto)
 print(info)
 
 # buscando grupos de padrões
-texto = 'ABCDefgHI123'
+texto1 = 'ABCDefgHI123'
 # [] - representa que queremos um conjunto de caracteres
-info = re.findall('[Ae3]',texto) # buscando 'A e 3'
-info2 = re.findall('[A-Z]',texto) # buscando de A a Z maiúsculo 
-info3 = re.findall('[a-z]',texto) # buscando de 'a' até 'z' minúsculo
-info4 = re.findall('[0-9]',texto) # buscando de 0 até 9
-info5 = re.findall('[A-Za-z]',texto) # buscando de A a Z maiúsculo e de a até z minúsculo
+info = re.findall('[Ae3]',texto1) # buscando 'A e 3'
+info2 = re.findall('[A-Z]',texto1) # buscando de A a Z maiúsculo 
+info3 = re.findall('[a-z]',texto1) # buscando de 'a' até 'z' minúsculo
+info4 = re.findall('[0-9]',texto1) # buscando de 0 até 9
+info5 = re.findall('[A-Za-z]',texto1) # buscando de A a Z maiúsculo e de a até z minúsculo
 print(info)
 print(info2)
 print(info3)
@@ -38,11 +38,10 @@ print(info4)
 print(info5)
 
 #buscando ocorrência de mais de um texto
-texto = 'existem 64 predios com 700 metros'
-info = re.findall('predio|metros',texto) # '|' significa OU
+texto2 = 'existem 64 predios com 700 metros'
+info = re.findall('predio|metros',texto2) # '|' significa OU
 print(info)
 
 # buscando caracteres e números com OU '|'
-texto = 'ABCDefgHI123'
-info = re.findall('[A-Z]|[0-9]',texto)
+info = re.findall('[A-Z]|[0-9]',texto1)
 print(info)
