5f0ca0fa225238029e1e27752977238dce516a06
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 14:35:49 -0300

    criando variável do tipo numpy bytes

--- formacao-python-udemy/numpy/tipos_de_dados.py
+++ formacao-python-udemy/numpy/tipos_de_dados.py
@@ -2,3 +2,6 @@ import numpy
 
 boolean = numpy.bool_(True) # criando variável do tipo numpy booleana
 print(boolean,type(boolean))
+
+string = numpy.bytes_("este e um texto") # criando variável do tipo numpy bytes
+print(string)
