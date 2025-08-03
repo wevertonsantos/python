fa6a0d22aa1480ab1bc492c22956a12d638845d2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 13:43:35 -0300

    exemplo de uso com data.

--- formacao-python-udemy/expressoes_regulares/exemplos_de_uso.py
+++ formacao-python-udemy/expressoes_regulares/exemplos_de_uso.py
@@ -15,3 +15,15 @@ if info != None:
     print("Número válido")
 else:
     print("Número inválido")
+
+# DD/MM/AAAA
+# O dia varia de 00 a 31
+# O mês de 00 a 12
+# o ano de 0000 a 9999
+data = '31/09/2002'
+info = re.search('^([0-2][0-9]|[3][0-1])/([0][1-9]|[1][0-2])/([0-9]){4}$',data)
+
+if info != None:
+    print('Data válida:',info.span())
+else:
+    print("Data inválida")
