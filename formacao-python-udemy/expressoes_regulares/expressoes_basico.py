6513b24cf6525c85fa558ab784a91e8ec8cf7c16
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 11:50:26 -0300

    buscando padrões com '[]' que presenta conjunto de caracteres

--- formacao-python-udemy/expressoes_regulares/expressoes_basico.py
+++ formacao-python-udemy/expressoes_regulares/expressoes_basico.py
@@ -22,3 +22,17 @@ print(info)
 # sub - substitui o que é encontrado pelo o que é passado
 info = re.sub("1","#",texto)
 print(info)
+
+# buscando grupos de padrões
+texto = 'ABCDefgHI123'
+# [] - representa que queremos um conjunto de caracteres
+info = re.findall('[Ae3]',texto) # buscando 'A e 3'
+info2 = re.findall('[A-Z]',texto) # buscando de A a Z maiúsculo 
+info3 = re.findall('[a-z]',texto) # buscando de 'a' até 'z' minúsculo
+info4 = re.findall('[0-9]',texto) # buscando de 0 até 9
+info5 = re.findall('[A-Za-z]',texto) # buscando de A a Z maiúsculo e de a até z minúsculo
+print(info)
+print(info2)
+print(info3)
+print(info4)
+print(info5)
