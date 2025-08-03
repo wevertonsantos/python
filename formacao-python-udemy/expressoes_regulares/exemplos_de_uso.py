15de56278fe2099460b3782134aaaeb5c2b26e4e
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-03 13:21:54 -0300

    exemplo de uso com número.

--- formacao-python-udemy/expressoes_regulares/exemplos_de_uso.py
+++ formacao-python-udemy/expressoes_regulares/exemplos_de_uso.py
@@ -7,3 +7,11 @@ if info != None:
     print("Temperatura válida")
 else:
     print("Temperatura inválida")
+
+telefone = '99224466'
+info = re.search('^99([0-9]{6})$',telefone)
+
+if info != None:
+    print("Número válido")
+else:
+    print("Número inválido")
