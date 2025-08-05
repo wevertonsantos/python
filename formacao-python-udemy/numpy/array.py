227c2257804c58e3a7cd165b9ea9d1ff4681b790
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-05 11:19:15 -0300

    operações com array

--- formacao-python-udemy/numpy/array.py
+++ formacao-python-udemy/numpy/array.py
@@ -135,3 +135,14 @@ print(array3)
 array = np.array([[1,2,3],[4,5,6],[7,8,9]])
 array = array.flatten() # transformando array em um array normal sem dimensões com flatten
 print(array)
+
+# operações com array
+array1 = np.array([1,2,3])
+array2 = np.array([2,4,6])
+
+print(array1 + array2)
+print(array1 - array2)
+print(array1 / array2)
+print(array1 * array2)
+print(array1 ** array2)
+print(array1 // array2)
