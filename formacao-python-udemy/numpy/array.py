324c651d5f98d67c696265dd1f27d3a1eb4b7b75
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-05 11:33:08 -0300

    comparação entre arrays

--- formacao-python-udemy/numpy/array.py
+++ formacao-python-udemy/numpy/array.py
@@ -146,3 +146,11 @@ print(array1 / array2)
 print(array1 * array2)
 print(array1 ** array2)
 print(array1 // array2)
+
+#comparação entre arrays
+array1 = np.array([10,20,30,5])
+array2 = np.array([20,40,10,5])
+print(array1 > array2)
+print(array1 == array2)
+print(array1 != array2)
+print(np.array_equal(array1,array2)) # verificando se array é igual ao outro
