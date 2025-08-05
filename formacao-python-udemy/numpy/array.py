9433b70a4f9810b3ecaac630349ef24bfe56e662
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-05 09:52:18 -0300

    iteração em arrays de mais dimensoes em coluna

--- formacao-python-udemy/numpy/array.py
+++ formacao-python-udemy/numpy/array.py
@@ -65,3 +65,8 @@ array = np.array([[1,2,3,4],[5,6,7,8]])
 for i in array:
     for j in i:
         print(j)
+
+#iteração em arrays de mais dimensoes em coluna
+array = np.array([[1,2,3,4],[5,6,7,8]])
+for i in np.nditer(array,order='F'):
+    print(i)
