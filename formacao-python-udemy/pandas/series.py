70d7c01107007b7d8e540460cf1c486a29090471
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-05 15:07:28 -0300

    atribuindo index para serie

--- formacao-python-udemy/pandas/series.py
+++ formacao-python-udemy/pandas/series.py
@@ -2,4 +2,11 @@ import pandas as pd
 series = pd.Series([1,2,3,4,5]) # criando uma série
 print(series)
 
-series = pd.Series([1,2,3,4,5],dtype='i4') # definindo tipo explicitamente
+series = pd.Series([1,2,3,4,5],dtype='i4') # definindo tipo 
+print(series)
+
+series = pd.Series([1,2,3,4,5],dtype='i4',name="Meus Números") # adicionando propriedade
+print(series)
+
+series = pd.Series([1,2,3,4,5],dtype='i4',name="Meus Números",index=["Um","Dois","Três","Quatro","Cinco"]) # atribuindo index para serie
+print(series)
