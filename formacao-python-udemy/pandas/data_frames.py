a3a1533eb3d8356691e55437ecbcde9215c2798c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-06 10:12:34 -0300

    criando data frame a partir da lista

--- formacao-python-udemy/pandas/data_frames.py
+++ formacao-python-udemy/pandas/data_frames.py
@@ -1,2 +1,7 @@
 import pandas as pd
 data_frame = pd.DataFrame() # criando um data frame
+
+nomes = ['Roger','Lucas','Cristiano','Maria']
+#criando data frame a partir da lista
+data_frame = pd.DataFrame(nomes)
+print(data_frame)
