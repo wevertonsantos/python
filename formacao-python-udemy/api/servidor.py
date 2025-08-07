28d5311c0f6cd875283b3b86828319d28a53991b
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-07 12:53:57 -0300

    fazendo cotação completa

--- formacao-python-udemy/api/servidor.py
+++ formacao-python-udemy/api/servidor.py
@@ -1,4 +1,4 @@
-from flask import jsonify, Flask
+from flask import jsonify, Flask,request
 from pyngrok import ngrok
 
 app = Flask(__name__) # criando aplicação flask
@@ -18,5 +18,21 @@ def cotacao():
 @app.route('/conversao/<float:val>')
 def conversao(val):
     return str(val * 5.34)
+
+@app.route('/cotacaocompleta',methods=['GET'])
+def cotacaocompleta():
+    argumentos = request.args # pegando argumentos quando passado as rotas
+    valor = float(argumentos.get('valor'))
+    mes = float(argumentos.get('mes'))
+
+    total = 0.0
+    if mes == 'Janeiro':
+        total = valor * 5.34
+    elif mes == 'Feveriro':
+        total = valor * 5.22
+    elif mes == 'Marco':
+        total = valor * 5.19
+
+    return str(total)
 
 app.run() #executando aplicação
