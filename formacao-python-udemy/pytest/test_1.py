35c6345a915917415b9bea6659dc0639868f9b1d
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-07 10:02:37 -0300

    @pytest.fixture # essa função irá ser executada sempre que inciar o teste

--- formacao-python-udemy/pytest/test_1.py
+++ formacao-python-udemy/pytest/test_1.py
@@ -1,8 +1,13 @@
 import pytest
 
-@pytest.mark.primarios # marcando testes primários
-def test_par():
-    assert 10 % 2 == 0 # assert é para avaliar uma condição quando pytest passar
+@pytest.fixture # essa função irá ser executada sempre que inciar o teste
+def retorna_valor():
+    return 10
 
-def test_par2():
-    assert 10 % 3 == 0 # fazendo com que gere erro
+@pytest.mark.primarios # marcando testes primários
+def test_par(retorna_valor):
+    assert retorna_valor % 2 == 0 # assert é para avaliar uma condição quando pytest passar
+
+@pytest.mark.primarios
+def test_par2(retorna_valor):
+    assert retorna_valor % 3 == 0 # fazendo com que gere erro
