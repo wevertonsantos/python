18ede591381f0f68704634bc8d6239fe5a98e1de
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-07 11:58:01 -0300

    rota para acessar o endereço e executar função

--- formacao-python-udemy/api/servidor.py
+++ formacao-python-udemy/api/servidor.py
@@ -4,4 +4,8 @@ from flask_ngrok import run_with_ngrok
 app = Flask(__name__) # criando aplicação flask
 run_with_ngrok(app) # rodar ngrok com aplicação flask
 
+@app.route('/cotacao/') # rota para acessar o endereço e executar função
+def cotacao():
+    return '5.34'
+
 app.run() #executando aplicação
