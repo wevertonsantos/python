2885753b72d3d954d6a057562f319386b758bbe7
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-07 12:41:31 -0300

    chamando métodos criados.

--- formacao-python-udemy/api/cliente.py
+++ formacao-python-udemy/api/cliente.py
@@ -1,3 +1,10 @@
 import requests
 
 x = requests.get('http://127.0.0.1:5000/cotacao/') # pegando cotacao com método get
+print(x.text)
+
+x = requests.get('http://127.0.0.1:5000/') # pegando cotacao com método get
+print(x.text)
+
+x = requests.get('http://127.0.0.1:5000/conversao/100.0')
+print(x.text)
