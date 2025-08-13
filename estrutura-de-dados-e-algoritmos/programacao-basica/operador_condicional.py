370bfd90e3b386070aa55c958973e54e94edbd87
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-13 19:38:28 -0300

    operador condicional

---
+++ estrutura-de-dados-e-algoritmos/programacao-basica/operador_condicional.py
@@ -0,0 +1,23 @@
+if 5 > 3:
+    print('5 é maior que 3')
+
+if 5 > 6:
+    print('5 é maior')
+else:
+    print('5 não é maior')
+
+n = 3
+if n == 4:
+    print('n é igual a 4')
+else:
+    if n == 3:
+        print('n é igual a 3')
+    else:
+        print('n não é igual a 4 nem 3')
+
+x = 2
+y = 3
+if (x > 1) and (y % 2 == 0):
+    print('x é maior que 1 e y é par')
+else:
+    print('Uma ou nenhuma das condições foram satisfeitas')
