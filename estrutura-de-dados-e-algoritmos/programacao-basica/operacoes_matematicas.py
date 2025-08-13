f523bef8360be7fb6cf084e8f5dde3b5c8f6817f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-13 19:07:41 -0300

    operacoes matematicas

---
+++ estrutura-de-dados-e-algoritmos/programacao-basica/operacoes_matematicas.py
@@ -0,0 +1,13 @@
+a = 5
+b = 3
+print(a,b)
+print(a+b)
+print('A soma é', a + b)
+print('A subtração é', a - b)
+print('A divisão é', a / b)
+print('A multiplicação é',a * b)
+print('O resto da divisão é',10 % 2)
+print('5 elevado a 3 é', 5**3)
+
+import math # importar funções matemáticas
+print(math.sqrt(81)) #raiz quadrada
