45f19d076a8f30b5aaed9c5df7d7ff2dd21edd47
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-13 18:40:21 -0300

    variáveis float

--- estrutura-de-dados-e-algoritmos/programacao-basica/variaveis_e_constantes.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/variaveis_e_constantes.py
@@ -4,3 +4,11 @@ numero_jogos = 14
 numero_convidados = 15
 print(numero)
 print(numero,numero_convidados)
+
+# variáveis float
+pi = 3.14
+numero_euler = 2.71
+escala_terremoto = -4.55
+print(pi)
+print(numero_euler)
+print(escala_terremoto)
