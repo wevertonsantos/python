051e94dd5d066746c296409008ed3a5f29ddba37
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-13 19:09:29 -0300

    utilizando round

--- estrutura-de-dados-e-algoritmos/programacao-basica/operacoes_matematicas.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/operacoes_matematicas.py
@@ -11,3 +11,11 @@ print('5 elevado a 3 é', 5**3)
 
 import math # importar funções matemáticas
 print(math.sqrt(81)) #raiz quadrada
+
+# arredondamento
+casos_doenca = 134
+numero_habitantes = 34432
+casos_por_habitantes = casos_doenca / numero_habitantes
+print(casos_por_habitantes)
+print(round(casos_por_habitantes,6)) # arredondamento para 6 casas
+print('O número de casos por habitante é de', round(casos_por_habitantes,4))
