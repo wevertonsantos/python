ce8a4ef091294ce96be165a760e39e459ab0c066
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-14 12:43:33 -0300

    estrutura de repetição for

---
+++ estrutura-de-dados-e-algoritmos/programacao-basica/estruturas_de_repeticao_for.py
@@ -0,0 +1,19 @@
+for numero in range(1,5):
+    print(numero)
+
+for numero in range(5,0,-1):
+    print(numero)
+
+soma = 0
+for numero in range(1,6):
+    soma = soma + numero
+print(soma)
+
+palavra = 'sorvete'
+for letra in palavra:
+    print(letra)
+
+for i in range(0,5):
+    print(i)
+    for j in range(0,3):
+        print(j)
