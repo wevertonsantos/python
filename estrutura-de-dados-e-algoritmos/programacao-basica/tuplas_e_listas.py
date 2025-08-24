2206cee32829b932baa9bc0c63f2c364b7ee40c2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-24 17:28:59 -0300

    relocando arquivo

--- estrutura-de-dados-e-algoritmos/programacao-basica/tuplas_e_listas.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/tuplas_e_listas.py
@@ -8,3 +8,9 @@ print(tupla.index('Canis familiaries')) # posição que está
 
 for elemento in tupla:
     print(elemento)
+
+#list
+lista = ['Homo sapiens','Canis familiaries','Felis catus']
+lista2 = ['Xenopus laevis','Ailuropoda melanoleuca']
+lista3 = lista + lista2
+print(lista3)
