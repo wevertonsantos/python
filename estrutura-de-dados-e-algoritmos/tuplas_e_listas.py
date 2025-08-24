ffab6d19eb885e6cd600991e07c0bab4ade9d8e0
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2025-08-24 17:27:20 -0300

    tupla

---
+++ estrutura-de-dados-e-algoritmos/tuplas_e_listas.py
@@ -0,0 +1,10 @@
+# tupla
+tupla = ('Homo sapiens','Canis familiaries','Felis catus')
+print(tupla)
+print(tupla[0])
+print(tupla[1])
+print(tupla[2])
+print(tupla.index('Canis familiaries')) # posição que está
+
+for elemento in tupla:
+    print(elemento)
