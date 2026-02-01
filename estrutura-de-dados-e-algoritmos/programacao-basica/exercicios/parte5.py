1eb0912d1d64a31fff731e4dec6e9f84129b1cae
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-01 18:34:17 -0300

    ex 2

--- estrutura-de-dados-e-algoritmos/programacao-basica/exercicios/parte5.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/exercicios/parte5.py
@@ -22,3 +22,34 @@ def resultado_conversao(temperatura_fahrenheit):
 c = temperatura_celsius()
 f = temperatura_fahrenheit(c)
 resultado_conversao(f)
+
+'''
+Efetuar o cálculo da quantidade de litros de combustível gasto em uma viagem, utilizando um automóvel que faz 12 Km por litro. Para obter o cálculo, o usuário deve fornecer o tempo gasto na viagem e a velocidade média durante ela. Desta forma, será possível obter a distância percorrida com a fórmula DISTANCIA = TEMPO * VELOCIDADE. Tendo o valor da distância, basta calcular a quantidade de litros de combustível utilizada na viagem, com a fórmula: LITROS_USADOS = DISTANCIA / 12. O programa deve apresentar os valores da velocidade média, tempo gasto na viagem, a distância percorrida e a quantidade de litros utilizada na viagem
+
+- Função para ler os valores (não recebe parâmetro e retorna os dois valores)
+- Função para calcular a distância (recebe como parâmetro o tempo e a velocidade e retorna a distância)
+- Função para calcular a quantidade de litros (recebe como parâmetro a distância e retorna os litros)
+- Função para apresentar o resultado (recebe como parâmetro os valores e somente imprime o resultado)
+'''
+
+def tempo_gasto_velocidade_media():
+    tempo_gasto = int(input("Digite o tempo gasto na viagem: "))
+    velocidade_media = int(input("Digite a velocidade média durante viagem: "))
+    return tempo_gasto,velocidade_media
+
+def calcular_distancia(tempo_gasto,velocidade_media):
+    distancia = tempo_gasto * velocidade_media
+    return distancia
+
+def calcular_quantidade_litros(distancia):
+    litros_usados = distancia / 12
+    return litros_usados
+
+tempo_gasto,velocidade_media = tempo_gasto_velocidade_media()
+distancia = calcular_distancia(tempo_gasto,velocidade_media)
+litros_usados = calcular_quantidade_litros(distancia)
+
+def mostrar_resultado(tempo_gasto,velocidade_media,distancia,litros_usados):
+    print(f"Tempo gasto: {tempo_gasto}, velocidade média: {velocidade_media}, distancia: {distancia} e litros usados: {litros_usados}")
+
+mostrar_resultado(tempo_gasto,velocidade_media,distancia,litros_usados)
