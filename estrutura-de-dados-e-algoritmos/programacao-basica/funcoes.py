46091e87cb403840168c3b53452fe361847d4a10
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-01 12:11:25 -0300

    função com passagem de parâmetro

--- estrutura-de-dados-e-algoritmos/programacao-basica/funcoes.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/funcoes.py
@@ -3,3 +3,14 @@ def mensagem():
     print('Texto da função')
 
 mensagem()
+
+#função com passagem de parâmetro
+def mensagem(texto):
+    print(texto)
+
+mensagem('Texto 1')
+
+def soma(a,b):
+    print(a + b)
+
+soma(2,2)
