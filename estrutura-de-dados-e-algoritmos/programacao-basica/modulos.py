fe08d70a5e52965f18c7de89579c0d87809aa615
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-01 18:45:04 -0300

    bibliotecas math e datetime

--- estrutura-de-dados-e-algoritmos/programacao-basica/modulos.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/modulos.py
@@ -15,3 +15,34 @@ print(math.log(32,2))
 print(math.log(1000))
 #pi
 print(math.pi)
+
+'''
+biblioteca datetime
+'''
+import datetime
+
+#recursos existente na biblioteca
+print(dir(datetime))
+#data de hoje
+print(datetime.date.today())
+#data e hora
+print(datetime.datetime.now())
+
+data = datetime.date(2026, 2,1)
+print(data)
+#dia
+print(data.day)
+#mes
+print(data.month)
+#ano
+print(data.year)
+
+horario = datetime.datetime(2026,2,1,18,43,0)
+print(horario)
+
+#hora
+print(horario.hour)
+#minuto
+print(horario.minute)
+#segundos
+print(horario.second)
