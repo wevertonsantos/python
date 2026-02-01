ef19d2e47ebb6687dc5d8c5396de887746770de6
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-01 18:40:14 -0300

    biblioteca math

---
+++ estrutura-de-dados-e-algoritmos/programacao-basica/modulos.py
@@ -0,0 +1,17 @@
+'''
+biblioteca math
+'''
+import math
+
+#raiz quadrada
+print(math.sqrt(9))
+#seno
+print(math.sin(45))
+#cosseno
+print(math.cos(45))
+#logaritmos
+print(math.log(1000,10))
+print(math.log(32,2))
+print(math.log(1000))
+#pi
+print(math.pi)
