c025a5bb3848e9b7e8612d9114c115c9cb36b168
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-02 14:32:54 -0300

    exceções

--- estrutura-de-dados-e-algoritmos/programacao-basica/tratamento_erro_excecoes.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/tratamento_erro_excecoes.py
@@ -5,5 +5,20 @@ RuntimeError: erro de execução
 SyntaxError: sintaxe digitada é inválida e não reconhecida pelo interpretador
 ZeroDivisionError: divisão por zero
 IndexError: índice está fora da coleção
+ValueError: valor inválido
+KeyboardInterrupt : botão para interromper
 '''
 
+try: 
+    n = int(input('Digite um número inteiro: '))
+except:
+    print('Valor inválido')
+else:
+    print(f'Valor digitado é: {n}')
+
+try: 
+    n = int(input('Digite um número inteiro: '))
+except ValueError:
+    print('Valor inválido')
+else:
+    print(f'Valor digitado é: {n}')
