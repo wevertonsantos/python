7c25c3f283870d3ada8c3b9f1796e905e6739ee2
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-02 14:25:29 -0300

    tipo de erros

---
+++ estrutura-de-dados-e-algoritmos/programacao-basica/tratamento_erro_excecoes.py
@@ -0,0 +1,9 @@
+'''
+NameError: variável nao foi definida
+TypeError: tipos de dados incompatíveis
+RuntimeError: erro de execução
+SyntaxError: sintaxe digitada é inválida e não reconhecida pelo interpretador
+ZeroDivisionError: divisão por zero
+IndexError: índice está fora da coleção
+'''
+
