0d3ef6336256e6f68464c112333d62779434e595
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-03 12:19:22 -0300

    metacaracteres e quantificadores

---
+++ estrutura-de-dados-e-algoritmos/programacao-basica/expressoes_regulares.py
@@ -0,0 +1,23 @@
+'''
+Metacaracteres:
+
+. - qualquer caractere (exceto linha nova)
+\w - qualquer caractere alfanumérico
+\W - qualquer caractere não-alfanumérico
+\d - qualquer caractere que seja um dígito (0-9)
+\D - qualquer caractere não dígito
+\s - espaço em branco
+^ - começa com
+$ - termina com
+\ - usado antes de metacaracteres para especificar seu significado literal
+
+Quantificadores - permitem determinar como e quantas vezes os metacaracteres aparecem
+
+[] - opcional entre os que estão dentro dos colchetes
+() - captura grupos de caracteres
+* - de zero a mais vezes
+? - zero ou uma vez
++ - uma ou mais vezes
+{m,n} - de m a n vezes
+| - ou
+'''
