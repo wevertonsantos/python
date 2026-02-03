813fc2639fd2bd3b58c228b68c71b83a7b3fee1a
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-03 12:39:30 -0300

    utilizando findall

--- estrutura-de-dados-e-algoritmos/programacao-basica/expressoes_regulares.py
+++ estrutura-de-dados-e-algoritmos/programacao-basica/expressoes_regulares.py
@@ -42,3 +42,17 @@ print(re.match('[A-Za-z]{3}-\d{4}',frase))
 
 frase2 = 'FRT-1998 é a placa do carro'
 print(re.match('[A-Za-z]{3}-\d{4}',frase2))
+
+#função findall - encontrar várias ocorrências
+
+frase3 = 'Meu número de telefone atual é (42)0000-0000. O número (56)1111-1111 é o antigo'
+print(re.findall('\(\d{2}\)\d{4,5}-\d{4}',frase3))
+
+emails = '''Nome: Teste 1
+email: teste1@teste.com
+Nome: Teste 2
+email: teste2@teste.com
+Nome: Teste 3
+email: teste3@teste.com.br
+'''
+print(re.findall('\w+@\w+\.\w+',emails))
