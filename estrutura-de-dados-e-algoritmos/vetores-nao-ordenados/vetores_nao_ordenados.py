c6e4b3f5dab034adba9134d7e3a076895639bf8c
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-05 15:28:36 -0300

    inserindo valor

--- estrutura-de-dados-e-algoritmos/vetores-nao-ordenados/vetores_nao_ordenados.py
+++ estrutura-de-dados-e-algoritmos/vetores-nao-ordenados/vetores_nao_ordenados.py
@@ -12,6 +12,18 @@ class VetorNaoOrdenado:
         else:
             for i in range(self.ultima_posicao + 1):
                 print(i, ' - ',self.valores[i])
+    
+    def insere(self,valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            print('Capacidade máxima atingida')
+        else:
+            self.ultima_posicao += 1
+            self.valores[self.ultima_posicao] = valor
 
 vetor = VetorNaoOrdenado(5)
+vetor.insere(2)
+vetor.insere(3)
+vetor.insere(5)
+vetor.insere(8)
+vetor.insere(1)
 vetor.imprime()
