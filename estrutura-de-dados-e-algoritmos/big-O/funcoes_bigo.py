d00a7fd03f47e91074fc2dfb53100feec49fbf03
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-05 14:50:43 -0300

    Big-O - Constant, linear, quadratic e combination

--- estrutura-de-dados-e-algoritmos/big-O/funcoes_bigo.py
+++ estrutura-de-dados-e-algoritmos/big-O/funcoes_bigo.py
@@ -14,4 +14,48 @@ for i in range(len(big_o)):
 plt.legend()
 plt.ylabel('Runtime')
 plt.xlabel('n')
-plt.savefig('graficos.png')
+#plt.savefig('graficos.png')
+
+#Constant - O(1)
+lista = [1,2,3,4,5]
+def constant(n):
+    print(n[0])
+constant(lista)
+
+#Linear - O(n)
+def linear(n):
+    for i in n:
+        print(i)
+linear(lista)
+
+#Quadratic - O(n^2) - polynomial
+def quadratic(n):
+    for i in n:
+        for j in n:
+            print(i,j)
+quadratic(lista)
+
+#Combination - O(1) + O(5) + O(n) + O(n) + O(3)
+# O(9) + O(2n) -> O(n)
+def combination(n):
+    # O(1)
+    print(n[0])
+
+    # O(5)
+    for i in range(5):
+        print('test ', i)
+
+    # O(n)
+    for i in n:
+        print(i)
+
+    # O(n)
+    for i in n:
+        print(i)
+
+    # O(3)
+    print('Python')
+    print('Python')
+    print('Python')
+
+combination(lista)
