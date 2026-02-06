a7ff6f1e6e02796a9fd8cae5dd2c551c0655993f
Weverton Santos <72475645+wevertonsantos@users.noreply.github.com>
2026-02-06 14:54:40 -0300

    pesquisar e inserir em vetores ordenados

---
+++ estrutura-de-dados-e-algoritmos/vetores/vetores_ordenados.py
@@ -0,0 +1,54 @@
+import numpy as np
+
+class VetorOrdenado:
+    def __init__(self,capacidade):
+        self.capacidade = capacidade
+        self.ultima_posicao = -1
+        self.valores = np.empty(self.capacidade, dtype=int)
+
+    def imprime(self):
+        if self.ultima_posicao == -1:
+            print('O vetor está vazio')
+        else:
+            for i in range(self.ultima_posicao + 1):
+                print(i, ' - ',self.valores[i])
+    
+    def insere(self,valor):
+        if self.ultima_posicao == self.capacidade - 1:
+            print('Capacidade máxima atingida')
+            return
+
+        posicao = 0
+        for i in range(self.ultima_posicao + 1):
+            posicao = i
+            if self.valores[i] > valor:
+                break
+            if i == self.ultima_posicao:
+                posicao = i + 1
+        
+        x = self.ultima_posicao
+        while x >= posicao:
+            self.valores[x + 1] = self.valores[x]
+            x -= 1
+
+        self.valores[posicao] = valor
+        self.ultima_posicao += 1
+    
+    def pesquisar(self,valor):
+        for i in range(self.ultima_posicao + 1):
+            if self.valores[i] > valor:
+                return -1
+            if self.valores[i] == valor:
+                return i
+            if i == self.ultima_posicao:
+                return -1
+
+vetor = VetorOrdenado(10)
+
+vetor.insere(6)
+vetor.insere(4)
+vetor.insere(3)
+vetor.insere(5)
+vetor.imprime()
+
+print(vetor.pesquisar(8))
